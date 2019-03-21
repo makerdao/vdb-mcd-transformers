e968d8d6dbccc64f8e98af4d9b7642fd5c73bff5
Elizabeth <emengelman@gmail.com>
2019-03-21 11:13:03 -0500

    Clean up (#22)
    * Alphabetize event names in config and constants
    
    * Remove unused constants

--- transformers/shared/constants/method.go
+++ transformers/shared/constants/method.go
@@ -7,17 +7,17 @@ func catFileFlipMethod() string        { return GetSolidityFunctionSignature(Cat
 func catFilePitVowMethod() string      { return "file(bytes32,address)" }
 func dealMethod() string               { return GetSolidityFunctionSignature(FlipperABI(), "deal") }
 func dentMethod() string               { return GetSolidityFunctionSignature(FlipperABI(), "dent") }
+func flapKickMethod() string           { return GetSolidityFunctionSignature(FlapperABI(), "Kick") }
+func flipKickMethod() string           { return GetSolidityFunctionSignature(FlipperABI(), "Kick") }
+func flopKickMethod() string           { return GetSolidityFunctionSignature(FlopperABI(), "Kick") }
 func jugDripMethod() string            { return GetSolidityFunctionSignature(JugABI(), "drip") }
 func jugFileIlkMethod() string         { return "file(bytes32,bytes32,uint256)" }
 func jugFileRepoMethod() string        { return "file(bytes32,uint256)" }
 func jugFileVowMethod() string         { return "file(bytes32,bytes32)" }
-func flapKickMethod() string           { return GetSolidityFunctionSignature(FlapperABI(), "Kick") }
-func flipKickMethod() string           { return GetSolidityFunctionSignature(FlipperABI(), "Kick") }
-func flopKickMethod() string           { return GetSolidityFunctionSignature(FlopperABI(), "Kick") }
 func logValueMethod() string           { return GetSolidityFunctionSignature(MedianizerABI(), "LogValue") }
+func tendMethod() string               { return GetSolidityFunctionSignature(FlipperABI(), "tend") }
 func vatFileDebtCeilingMethod() string { return "file(bytes32,uint256)" }
 func vatFileIlkMethod() string         { return "file(bytes32,bytes32,uint256)" }
-func tendMethod() string               { return GetSolidityFunctionSignature(FlipperABI(), "tend") }
 func vatFluxMethod() string            { return GetSolidityFunctionSignature(OldVatABI(), "flux") }
 func vatFoldMethod() string            { return GetSolidityFunctionSignature(OldVatABI(), "fold") }
 func vatFrobMethod() string            { return GetSolidityFunctionSignature(VatABI(), "frob") }
