b61530442753def877a5527828ac189bb191fa34
Rob Mulholand <rmulholand@8thlight.com>
2019-03-28 10:33:19 -0500

    Fix Travis
    - Add setup from vDB - encrypted Infura token

--- transformers/integration_tests/integration_tests_suite_test.go
+++ transformers/integration_tests/integration_tests_suite_test.go
@@ -1,6 +1,7 @@
 package integration_tests
 
 import (
+	"errors"
 	log "github.com/sirupsen/logrus"
 	"testing"
 
@@ -17,12 +18,20 @@ func TestIntegrationTests(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	testConfig := viper.New()
-	testConfig.SetConfigName("staging")
+	testConfig.SetConfigName("infura")
 	testConfig.AddConfigPath("$GOPATH/src/github.com/vulcanize/mcd_transformers/environments/")
 	err := testConfig.ReadInConfig()
 	ipc = testConfig.GetString("client.ipcPath")
 	if err != nil {
 		log.Fatal(err)
+	}
+	// If we don't have an ipc path in the config file, check the env variable
+	if ipc == "" {
+		testConfig.BindEnv("url", "INFURA_URL")
+		ipc = testConfig.GetString("url")
+	}
+	if ipc == "" {
+		log.Fatal(errors.New("infura.toml IPC path or $INFURA_URL env variable need to be set"))
 	}
 	log.SetOutput(ioutil.Discard)
 })
