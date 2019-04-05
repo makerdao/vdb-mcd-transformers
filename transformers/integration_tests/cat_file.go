db34cac74e6bf9085844f3b3b984c0536a02c97c
Rob Mulholand <rmulholand@8thlight.com>
2019-04-05 09:16:12 -0500

    Update config to point at MCD_CAT

--- transformers/integration_tests/cat_file.go
+++ transformers/integration_tests/cat_file.go
@@ -292,4 +292,3 @@ type byLogIndexChopLump []chop_lump.CatFileChopLumpModel
 func (c byLogIndexChopLump) Len() int           { return len(c) }
 func (c byLogIndexChopLump) Less(i, j int) bool { return c[i].LogIndex < c[j].LogIndex }
 func (c byLogIndexChopLump) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-
