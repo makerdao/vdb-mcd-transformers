f898f8a8c4230ed3ca28c0b1d86c25b10f3648ef
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2019-05-08 14:26:03 -0700

    Fixes migration with WHERE clause

--- db/migrations/20190507012413_create_logValue_query.sql
+++ db/migrations/20190507012413_create_logValue_query.sql
@@ -1,34 +1,38 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 CREATE TYPE maker.log_value AS (
-  val         NUMERIC
+  val           NUMERIC,
+  block_number  BIGINT,
+  tx_idx        INTEGER
   -- tx
   );
-
 
 
 CREATE OR REPLACE FUNCTION maker.log_values(beginTime INT, endTime INT)
   RETURNS SETOF maker.log_value AS
 $body$
-SELECT val FROM maker.pip_log_value
-                  LEFT JOIN public.headers ON pip_log_value.header_id = headers.id
-WHERE block_timestamp BETWEEN $1 AND $2
+  SELECT val, pip_log_value.block_number, tx_idx FROM maker.pip_log_value
+    LEFT JOIN public.headers ON pip_log_value.header_id = headers.id
+    WHERE block_timestamp BETWEEN $1 AND $2
 $body$
   LANGUAGE sql STABLE;
-
 
 
 CREATE OR REPLACE FUNCTION maker.log_value_tx(priceUpdate maker.log_value)
   RETURNS maker.tx AS
 $body$
-SELECT txs.hash, txs.tx_index, headers.block_number, headers.hash, txs.tx_from, txs.tx_to
-FROM public.header_sync_transactions txs
-       LEFT JOIN headers ON txs.header_id = headers.id
-ORDER BY block_number DESC
+  SELECT txs.hash, txs.tx_index, headers.block_number, headers.hash, txs.tx_from, txs.tx_to
+    FROM public.header_sync_transactions txs
+    LEFT JOIN headers ON txs.header_id = headers.id
+    LEFT JOIN maker.pip_log_value plv ON txs.header_id = plv.header_id
+    WHERE headers.block_number = priceUpdate.block_number
+    ORDER BY headers.block_number DESC
 $body$
   LANGUAGE sql STABLE;
+
+
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back
-DROP FUNCTION IF EXISTS makerlog_values(INT, INT);
+DROP FUNCTION IF EXISTS maker.log_values(INT, INT);
 DROP FUNCTION maker.log_value_tx(maker.log_value);
 DROP TYPE maker.log_value CASCADE;
