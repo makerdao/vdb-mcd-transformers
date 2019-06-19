7e3148e0c6f3ac8cc50de1eae9f6c5798a96858e
Gabe Laughlin <gabe@enok.co>
2019-06-18 21:32:06 -0500

    (VDB-682) Update price feed query to use spot poke

---
+++ db/migrations/20190507012413_create_poke_events_query.sql
@@ -0,0 +1,44 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TYPE api.poke_event AS (
+    ilk_id INTEGER,
+    -- ilk object
+    val NUMERIC,
+    spot NUMERIC,
+    block_height BIGINT,
+    tx_idx INTEGER
+    -- tx
+    );
+
+COMMENT ON COLUMN api.poke_event.ilk_id
+    IS E'@omit';
+COMMENT ON COLUMN api.poke_event.block_height
+    IS E'@omit';
+COMMENT ON COLUMN api.poke_event.tx_idx
+    IS E'@omit';
+
+CREATE FUNCTION api.max_timestamp()
+    RETURNS NUMERIC AS
+$$
+SELECT max(block_timestamp)
+FROM public.headers
+$$
+    LANGUAGE SQL
+    STABLE;
+
+CREATE FUNCTION api.all_poke_events(beginTime NUMERIC DEFAULT 0, endTime NUMERIC DEFAULT api.max_timestamp())
+    RETURNS SETOF api.poke_event AS
+$body$
+SELECT ilk_id, "value" AS val, spot, block_number AS block_height, tx_idx
+FROM maker.spot_poke
+         LEFT JOIN public.headers ON spot_poke.header_id = headers.id
+WHERE block_timestamp BETWEEN beginTime AND endTime
+$body$
+    LANGUAGE sql
+    STABLE;
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back
+DROP FUNCTION api.all_poke_events(NUMERIC, NUMERIC);
+DROP FUNCTION api.max_timestamp();
+DROP TYPE api.poke_event CASCADE;
---
+++ transformers/storage/spot/initializer/initializer.go
@@ -0,0 +1,17 @@
+package initializer
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/vulcanize/vulcanizedb/libraries/shared/factories/storage"
+	"github.com/vulcanize/vulcanizedb/libraries/shared/transformer"
+
+	"github.com/vulcanize/mcd_transformers/transformers/shared/constants"
+	storage2 "github.com/vulcanize/mcd_transformers/transformers/storage"
+	"github.com/vulcanize/mcd_transformers/transformers/storage/spot"
+)
+
+var StorageTransformerInitializer transformer.StorageTransformerInitializer = storage.Transformer{
+	Address:    common.HexToAddress(constants.SpotContractAddress()),
+	Mappings:   &spot.SpotMappings{StorageRepository: &storage2.MakerStorageRepository{}},
+	Repository: &spot.SpotStorageRepository{},
+}.NewTransformer
