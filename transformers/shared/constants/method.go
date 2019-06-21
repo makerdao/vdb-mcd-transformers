0faddbdb010c1d703122c84eb652b2346267246e
Gabe Laughlin <gabe@enok.co>
2019-06-21 00:19:37 -0500

    (VDB-613) Check that overloaded method signature exists in ABI

--- transformers/shared/constants/method.go
+++ transformers/shared/constants/method.go
@@ -16,37 +16,56 @@
 
 package constants
 
-//TODO: get cat and jug file method signatures directly from the ABI
-func biteMethod() string               { return getSolidityFunctionSignature(CatABI(), "Bite") }
-func catFileChopLumpMethod() string    { return "file(bytes32,bytes32,uint256)" }
-func catFileFlipMethod() string        { return getSolidityFunctionSignature(CatABI(), "file") }
-func catFileVowMethod() string         { return "file(bytes32,address)" }
-func dealMethod() string               { return getSolidityFunctionSignature(FlipperABI(), "deal") }
-func dentMethod() string               { return getSolidityFunctionSignature(FlipperABI(), "dent") }
-func flapKickMethod() string           { return getSolidityFunctionSignature(FlapperABI(), "Kick") }
-func flipKickMethod() string           { return getSolidityFunctionSignature(FlipperABI(), "Kick") }
-func flopKickMethod() string           { return getSolidityFunctionSignature(FlopperABI(), "Kick") }
-func jugDripMethod() string            { return getSolidityFunctionSignature(JugABI(), "drip") }
-func jugFileBaseMethod() string        { return "file(bytes32,uint256)" }
-func jugFileIlkMethod() string         { return "file(bytes32,bytes32,uint256)" }
-func jugFileVowMethod() string         { return "file(bytes32,address)" }
-func jugInitMethod() string            { return getSolidityFunctionSignature(JugABI(), "init") }
-func spotFileMatMethod() string        { return "file(bytes32,bytes32,uint256)" }
-func spotFilePipMethod() string        { return "file(bytes32,address)" }
-func spotPokeMethod() string           { return getSolidityFunctionSignature(SpotABI(), "Poke") }
-func tendMethod() string               { return getSolidityFunctionSignature(FlipperABI(), "tend") }
-func vatFileDebtCeilingMethod() string { return "file(bytes32,uint256)" }
-func vatFileIlkMethod() string         { return "file(bytes32,bytes32,uint256)" }
-func vatFluxMethod() string            { return getSolidityFunctionSignature(VatABI(), "flux") }
-func vatFoldMethod() string            { return getSolidityFunctionSignature(VatABI(), "fold") }
-func vatForkMethod() string            { return getSolidityFunctionSignature(VatABI(), "fork") }
-func vatFrobMethod() string            { return getSolidityFunctionSignature(VatABI(), "frob") }
-func vatGrabMethod() string            { return getSolidityFunctionSignature(VatABI(), "grab") }
-func vatHealMethod() string            { return getSolidityFunctionSignature(VatABI(), "heal") }
-func vatInitMethod() string            { return getSolidityFunctionSignature(VatABI(), "init") }
-func vatMoveMethod() string            { return getSolidityFunctionSignature(VatABI(), "move") }
-func vatSlipMethod() string            { return getSolidityFunctionSignature(VatABI(), "slip") }
-func vatSuckMethod() string            { return getSolidityFunctionSignature(VatABI(), "suck") }
-func vowFessMethod() string            { return getSolidityFunctionSignature(VowABI(), "fess") }
-func vowFileMethod() string            { return getSolidityFunctionSignature(VowABI(), "file") }
-func vowFlogMethod() string            { return getSolidityFunctionSignature(VowABI(), "flog") }
+func biteMethod() string { return getSolidityFunctionSignature(CatABI(), "Bite") }
+func catFileChopLumpMethod() string {
+	return getOverloadedFunctionSignature(CatABI(), "file", []string{"bytes32", "bytes32", "uint256"})
+}
+func catFileFlipMethod() string {
+	return getOverloadedFunctionSignature(CatABI(), "file", []string{"bytes32", "bytes32", "address"})
+}
+func catFileVowMethod() string {
+	return getOverloadedFunctionSignature(CatABI(), "file", []string{"bytes32", "address"})
+}
+func dealMethod() string     { return getSolidityFunctionSignature(FlipperABI(), "deal") }
+func dentMethod() string     { return getSolidityFunctionSignature(FlipperABI(), "dent") }
+func flapKickMethod() string { return getSolidityFunctionSignature(FlapperABI(), "Kick") }
+func flipKickMethod() string { return getSolidityFunctionSignature(FlipperABI(), "Kick") }
+func flopKickMethod() string { return getSolidityFunctionSignature(FlopperABI(), "Kick") }
+func jugDripMethod() string  { return getSolidityFunctionSignature(JugABI(), "drip") }
+func jugFileBaseMethod() string {
+	return getOverloadedFunctionSignature(JugABI(), "file", []string{"bytes32", "uint256"})
+}
+func jugFileIlkMethod() string {
+	return getOverloadedFunctionSignature(JugABI(), "file", []string{"bytes32", "bytes32", "uint256"})
+}
+func jugFileVowMethod() string {
+	return getOverloadedFunctionSignature(JugABI(), "file", []string{"bytes32", "address"})
+}
+func jugInitMethod() string { return getSolidityFunctionSignature(JugABI(), "init") }
+func spotFileMatMethod() string {
+	return getOverloadedFunctionSignature(SpotABI(), "file", []string{"bytes32", "bytes32", "uint256"})
+}
+func spotFilePipMethod() string {
+	return getOverloadedFunctionSignature(SpotABI(), "file", []string{"bytes32", "address"})
+}
+func spotPokeMethod() string { return getSolidityFunctionSignature(SpotABI(), "Poke") }
+func tendMethod() string     { return getSolidityFunctionSignature(FlipperABI(), "tend") }
+func vatFileDebtCeilingMethod() string {
+	return getOverloadedFunctionSignature(VatABI(), "file", []string{"bytes32", "uint256"})
+}
+func vatFileIlkMethod() string {
+	return getOverloadedFunctionSignature(VatABI(), "file", []string{"bytes32", "bytes32", "uint256"})
+}
+func vatFluxMethod() string { return getSolidityFunctionSignature(VatABI(), "flux") }
+func vatFoldMethod() string { return getSolidityFunctionSignature(VatABI(), "fold") }
+func vatForkMethod() string { return getSolidityFunctionSignature(VatABI(), "fork") }
+func vatFrobMethod() string { return getSolidityFunctionSignature(VatABI(), "frob") }
+func vatGrabMethod() string { return getSolidityFunctionSignature(VatABI(), "grab") }
+func vatHealMethod() string { return getSolidityFunctionSignature(VatABI(), "heal") }
+func vatInitMethod() string { return getSolidityFunctionSignature(VatABI(), "init") }
+func vatMoveMethod() string { return getSolidityFunctionSignature(VatABI(), "move") }
+func vatSlipMethod() string { return getSolidityFunctionSignature(VatABI(), "slip") }
+func vatSuckMethod() string { return getSolidityFunctionSignature(VatABI(), "suck") }
+func vowFessMethod() string { return getSolidityFunctionSignature(VowABI(), "fess") }
+func vowFileMethod() string { return getSolidityFunctionSignature(VowABI(), "file") }
+func vowFlogMethod() string { return getSolidityFunctionSignature(VowABI(), "flog") }
