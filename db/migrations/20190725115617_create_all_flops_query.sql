eabf11f97e938722ce84ef07821f2ec72867de5d
Amanda <aaizuss@8thlight.com>
2019-07-30 16:00:09 -0700

    VDB-722 Create all flops query (#161)

---
+++ db/migrations/20190725115617_create_all_flops_query.sql
@@ -0,0 +1,35 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION api.all_flops()
+    RETURNS SETOF api.flop
+AS
+$BODY$
+BEGIN
+    RETURN QUERY (
+        WITH bid_ids AS (
+            SELECT DISTINCT flop_bid_guy.bid_id
+            FROM maker.flop_bid_guy
+            UNION
+            SELECT DISTINCT flop_bid_tic.bid_id
+            FROM maker.flop_bid_tic
+            UNION
+            SELECT DISTINCT flop_bid_bid.bid_id
+            FROM maker.flop_bid_bid
+            UNION
+            SELECT DISTINCT flop_bid_lot.bid_id
+            FROM maker.flop_bid_lot
+            UNION
+            SELECT DISTINCT flop_bid_end.bid_id
+            FROM maker.flop_bid_end
+        )
+        SELECT f.*
+        FROM bid_ids,
+             LATERAL api.get_flop(bid_ids.bid_id) f
+    );
+END
+$BODY$
+    LANGUAGE plpgsql;
+-- +goose StatementEnd
+-- +goose Down
+DROP FUNCTION api.all_flops();
+
---
+++ transformers/component_tests/queries/all_flops_query_test.go
@@ -0,0 +1,91 @@
+package queries
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/vulcanize/mcd_transformers/test_config"
+	"github.com/vulcanize/mcd_transformers/transformers/component_tests/queries/test_helpers"
+	"github.com/vulcanize/mcd_transformers/transformers/events/deal"
+	"github.com/vulcanize/mcd_transformers/transformers/storage/flop"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres/repositories"
+	"github.com/vulcanize/vulcanizedb/pkg/fakes"
+	"math/rand"
+	"strconv"
+)
+
+var _ = Describe("All flops query", func() {
+	var (
+		db              *postgres.DB
+		flopRepo        flop.FlopStorageRepository
+		dealRepo        deal.DealRepository
+		headerRepo      repositories.HeaderRepository
+		contractAddress = "contract address"
+
+		blockOne          = rand.Int()
+		blockOneTimestamp = int64(111111111)
+
+		blockTwo          = blockOne + 1
+		blockTwoTimestamp = int64(222222222)
+	)
+
+	BeforeEach(func() {
+		db = test_config.NewTestDB(test_config.NewTestNode())
+		test_config.CleanTestDB(db)
+		flopRepo = flop.FlopStorageRepository{}
+		flopRepo.SetDB(db)
+		dealRepo = deal.DealRepository{}
+		dealRepo.SetDB(db)
+		headerRepo = repositories.NewHeaderRepository(db)
+	})
+
+	AfterEach(func() {
+		closeErr := db.Close()
+		Expect(closeErr).NotTo(HaveOccurred())
+	})
+
+	It("gets the most recent flop for every bid id", func() {
+		fakeBidIdOne := rand.Int()
+		fakeBidIdTwo := fakeBidIdOne + 1
+
+		blockOneHeader := fakes.GetFakeHeaderWithTimestamp(blockOneTimestamp, int64(blockOne))
+		_, headerOneErr := headerRepo.CreateOrUpdateHeader(blockOneHeader)
+		Expect(headerOneErr).NotTo(HaveOccurred())
+
+		blockTwoHeader := fakes.GetFakeHeaderWithTimestamp(blockTwoTimestamp, int64(blockTwo))
+		blockTwoHeader.Hash = "blockTwoHeader"
+		_, headerTwoErr := headerRepo.CreateOrUpdateHeader(blockTwoHeader)
+		Expect(headerTwoErr).NotTo(HaveOccurred())
+
+		initialFlopOneStorageValues := test_helpers.GetFlopStorageValues(1, fakeBidIdOne)
+		test_helpers.CreateFlop(db, blockOneHeader, initialFlopOneStorageValues, test_helpers.GetFlopMetadatas(strconv.Itoa(fakeBidIdOne)), contractAddress)
+
+		updatedFlopOneStorageValues := test_helpers.GetFlopStorageValues(2, fakeBidIdOne)
+		test_helpers.CreateFlop(db, blockTwoHeader, updatedFlopOneStorageValues, test_helpers.GetFlopMetadatas(strconv.Itoa(fakeBidIdOne)), contractAddress)
+
+		flopBidContextErr := test_helpers.SetUpFlopBidContext(test_helpers.FlopBidSetupData{
+			Db:              db,
+			BidId:           fakeBidIdOne,
+			ContractAddress: contractAddress,
+			Dealt:           false,
+		})
+		Expect(flopBidContextErr).NotTo(HaveOccurred())
+
+		flopStorageValuesTwo := test_helpers.GetFlopStorageValues(3, fakeBidIdTwo)
+		test_helpers.CreateFlop(db, blockTwoHeader, flopStorageValuesTwo, test_helpers.GetFlopMetadatas(strconv.Itoa(fakeBidIdTwo)), contractAddress)
+
+		var actualBids []test_helpers.FlopBid
+		queryErr := db.Select(&actualBids, `SELECT bid_id, guy, tic, "end", lot, bid, dealt, created, updated FROM api.all_flops()`)
+		Expect(queryErr).NotTo(HaveOccurred())
+
+		expectedBidOne := test_helpers.FlopBidFromValues(strconv.Itoa(fakeBidIdOne), "false", blockTwoHeader.Timestamp, blockOneHeader.Timestamp, updatedFlopOneStorageValues)
+		expectedBidTwo := test_helpers.FlopBidFromValues(strconv.Itoa(fakeBidIdTwo), "false", blockTwoHeader.Timestamp, blockTwoHeader.Timestamp, flopStorageValuesTwo)
+
+		Expect(len(actualBids)).To(Equal(2))
+		Expect(actualBids).To(ConsistOf([]test_helpers.FlopBid{
+			expectedBidOne,
+			expectedBidTwo,
+		}))
+	})
+
+})
