2a81a74301e933677d534cecc6446d9fcb165954
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2019-08-21 23:12:45 -0700

    Addresses feedback in PR comments
     - add index to yank bid id
     - use Amanda's DB function for tx_data
     - remove random seed generator
     - clarify test name
     - change test query to select count and assert zero rows

--- db/migrations/20190814205243_create_flop_bid_event_computed_columns.sql
+++ db/migrations/20190814205243_create_flop_bid_event_computed_columns.sql
@@ -14,12 +14,7 @@ $$
 CREATE FUNCTION api.flop_bid_event_tx(event api.flop_bid_event)
     RETURNS SETOF api.tx AS
 $$
-SELECT txs.hash, txs.tx_index, headers.block_number, headers.hash, tx_from, tx_to
-FROM public.header_sync_transactions txs
-         LEFT JOIN headers ON txs.header_id = headers.id
-WHERE block_number <= event.block_height
-  AND txs.tx_index <= event.tx_idx
-ORDER BY block_number DESC
+SELECT * FROM get_tx_data(event.block_height, event.tx_idx)
 $$
     LANGUAGE SQL
     STABLE;
