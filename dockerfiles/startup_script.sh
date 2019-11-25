5b2d2c306d08f281eb4b36a16b915a1f1fce33bb
Elizabeth Engelman <emengelman@gmail.com>
2019-11-25 09:50:22 -0600

    Terminate the container if one of the processes has exited

--- dockerfiles/startup_script.sh
+++ dockerfiles/startup_script.sh
@@ -37,11 +37,29 @@ then
 fi
 
 
+echo "Starting headerSync and executing the transformers..."
 # Fire up the services
 if [ $? -eq 0 ]; then
   # Fire up the services
   ./vulcanizedb headerSync --config config.toml -s $STARTING_BLOCK_NUMBER &
   ./vulcanizedb execute --config config.toml &
 fi
+
+
+# Check every 60 seconds to see if either process has excited.
+# If grepping for process names finds something, they exit with 0 status. If they are not both 0, then one of the processes has already excited.
+
+while sleep 60; do
+  ps aux | grep headerSync | grep -q -v grep
+  HEADER_SYNC_STATUS=$?
+
+  ps aux | grep execute | grep -q -v grep
+  EXECUTE_STATUS=$?
+
+  if [ $HEADER_SYNC_STATUS -ne 0 -o $EXECUTE_STATUS -ne 0 ]; then
+    echo "One of the processes has already exited."
+    exit 1
+  fi
+done
 
 wait
