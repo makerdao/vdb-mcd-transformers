vulcanize/mcd_transformers

Use vdb InsertionModel for cat_file_vow converter

transformers/events/cat_file/vow/converter.go
@@ -20,18 +20,25 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/vulcanize/mcd_transformers/transformers/shared"
 	"github.com/vulcanize/mcd_transformers/transformers/shared/constants"
+	"github.com/vulcanize/vulcanizedb/libraries/shared/factories/event"
 	"github.com/vulcanize/vulcanizedb/pkg/core"
+	"github.com/vulcanize/vulcanizedb/pkg/datastore/postgres"
 )
 
-type CatFileVowConverter struct{}
+type Converter struct{
+	db *postgres.DB
+}
+
 
 const (
 	logDataRequired   = true
 	numTopicsRequired = 4
+	What event.ColumnName = "what"
+	Data event.ColumnName = "data"
 )
 
-func (CatFileVowConverter) ToModels(_ string, logs []core.HeaderSyncLog) ([]shared.InsertionModel, error) {
-	var results []shared.InsertionModel
+func (Converter) ToModels(_ string, logs []core.HeaderSyncLog) ([]event.InsertionModel, error) {
+	var results []event.InsertionModel
 	for _, log := range logs {
 		err := shared.VerifyLog(log.Log, numTopicsRequired, logDataRequired)
 		if err != nil {
@@ -41,21 +48,25 @@ func (CatFileVowConverter) ToModels(_ string, logs []core.HeaderSyncLog) ([]shar
 		what := shared.DecodeHexToText(log.Log.Topics[2].Hex())
 		data := common.BytesToAddress(log.Log.Topics[3].Bytes()).String()
 
-		result := shared.InsertionModel{
+		result := event.InsertionModel{
 			SchemaName: "maker",
 			TableName:  "cat_file_vow",
-			OrderedColumns: []string{
-				constants.HeaderFK, "what", "data", constants.LogFK,
+			OrderedColumns: []event.ColumnName{
+				constants.HeaderFK, What, Data, event.LogFK,
 			},
-			ColumnValues: shared.ColumnValues{
-				"what":             what,
-				"data":             data,
+			ColumnValues:   event.ColumnValues{
 				constants.HeaderFK: log.HeaderID,
-				constants.LogFK:    log.ID,
+				What: what,
+				Data: data,
+				event.LogFK: log.ID,
 			},
-			ForeignKeyValues: shared.ForeignKeyValues{},
 		}
 		results = append(results, result)
 	}
 	return results, nil
 }
+
+func (converter *Converter) SetDB(db *postgres.DB) {
+	converter.db = db
+}
+

transformers/events/cat_file/vow/converter_test.go
@@ -22,18 +22,18 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/vulcanize/mcd_transformers/transformers/shared/constants"
+	"github.com/vulcanize/vulcanizedb/libraries/shared/factories/event"
 	"github.com/vulcanize/vulcanizedb/pkg/core"
 
 	"github.com/vulcanize/mcd_transformers/transformers/events/cat_file/vow"
-	"github.com/vulcanize/mcd_transformers/transformers/shared"
 	"github.com/vulcanize/mcd_transformers/transformers/test_data"
 )
 
 var _ = Describe("Cat file vow converter", func() {
-	var converter vow.CatFileVowConverter
+	var converter vow.Converter
 
 	BeforeEach(func() {
-		converter = vow.CatFileVowConverter{}
+		converter = vow.Converter{}
 	})
 
 	It("returns err if log is missing topics", func() {
@@ -62,6 +62,6 @@ var _ = Describe("Cat file vow converter", func() {
 		models, err := converter.ToModels(constants.CatABI(), []core.HeaderSyncLog{test_data.CatFileVowHeaderSyncLog})
 
 		Expect(err).NotTo(HaveOccurred())
-		Expect(models).To(Equal([]shared.InsertionModel{test_data.CatFileVowModel()}))
+		Expect(models).To(Equal([]event.InsertionModel{test_data.CatFileVowModel()}))
 	})
 })

transformers/events/cat_file/vow/initializer/initializer.go
@@ -18,14 +18,15 @@ package initializer
 
 import (
 	"github.com/vulcanize/mcd_transformers/transformers/shared/constants"
+	"github.com/vulcanize/vulcanizedb/libraries/shared/factories/event"
 	"github.com/vulcanize/vulcanizedb/libraries/shared/transformer"
 
 	"github.com/vulcanize/mcd_transformers/transformers/events/cat_file/vow"
 	"github.com/vulcanize/mcd_transformers/transformers/shared"
 )
 
-var EventTransformerInitializer transformer.EventTransformerInitializer = shared.EventTransformer{
+var EventTransformerInitializer transformer.EventTransformerInitializer = event.Transformer{
 	Config:     shared.GetEventTransformerConfig(constants.CatFileVowLabel, constants.CatFileVowSignature()),
-	Converter:  &vow.CatFileVowConverter{},
+	Converter:  &vow.Converter{},
 	Repository: &vow.CatFileVowRepository{},
-}.NewEventTransformer
+}.NewTransformer

makerdao/vdb-mcd-transformers

transformers/events/cat_file/vow/converter.go
@@ -20,18 +20,25 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	"github.com/makerdao/vulcanizedb/pkg/core"
+	"github.com/makerdao/vulcanizedb/pkg/datastore/postgres"
 )
 
-type CatFileVowConverter struct{}
+type Converter struct{
+	db *postgres.DB
+}
+
 
 const (
 	logDataRequired   = true
 	numTopicsRequired = 4
+	What event.ColumnName = "what"
+	Data event.ColumnName = "data"
 )
 
-func (CatFileVowConverter) ToModels(_ string, logs []core.HeaderSyncLog) ([]shared.InsertionModel, error) {
-	var results []shared.InsertionModel
+func (Converter) ToModels(_ string, logs []core.HeaderSyncLog) ([]event.InsertionModel, error) {
+	var results []event.InsertionModel
 	for _, log := range logs {
 		err := shared.VerifyLog(log.Log, numTopicsRequired, logDataRequired)
 		if err != nil {
@@ -41,21 +48,25 @@ func (CatFileVowConverter) ToModels(_ string, logs []core.HeaderSyncLog) ([]shar
 		what := shared.DecodeHexToText(log.Log.Topics[2].Hex())
 		data := common.BytesToAddress(log.Log.Topics[3].Bytes()).String()
 
-		result := shared.InsertionModel{
+		result := event.InsertionModel{
 			SchemaName: "maker",
 			TableName:  "cat_file_vow",
-			OrderedColumns: []string{
-				constants.HeaderFK, "what", "data", constants.LogFK,
+			OrderedColumns: []event.ColumnName{
+				constants.HeaderFK, What, Data, event.LogFK,
 			},
-			ColumnValues: shared.ColumnValues{
-				"what":             what,
-				"data":             data,
+			ColumnValues:   event.ColumnValues{
 				constants.HeaderFK: log.HeaderID,
-				constants.LogFK:    log.ID,
+				What: what,
+				Data: data,
+				event.LogFK: log.ID,
 			},
-			ForeignKeyValues: shared.ForeignKeyValues{},
 		}
 		results = append(results, result)
 	}
 	return results, nil
 }
+
+func (converter *Converter) SetDB(db *postgres.DB) {
+	converter.db = db
+}
+

transformers/events/cat_file/vow/converter_test.go
@@ -19,21 +19,20 @@ package vow_test
 import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/events/cat_file/vow"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/test_data"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	"github.com/makerdao/vulcanizedb/pkg/core"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/makerdao/vdb-mcd-transformers/transformers/events/cat_file/vow"
-	"github.com/makerdao/vdb-mcd-transformers/transformers/shared"
-	"github.com/makerdao/vdb-mcd-transformers/transformers/test_data"
 )
 
 var _ = Describe("Cat file vow converter", func() {
-	var converter vow.CatFileVowConverter
+	var converter vow.Converter
 
 	BeforeEach(func() {
-		converter = vow.CatFileVowConverter{}
+		converter = vow.Converter{}
 	})
 
 	It("returns err if log is missing topics", func() {
@@ -62,6 +61,6 @@ var _ = Describe("Cat file vow converter", func() {
 		models, err := converter.ToModels(constants.CatABI(), []core.HeaderSyncLog{test_data.CatFileVowHeaderSyncLog})
 
 		Expect(err).NotTo(HaveOccurred())
-		Expect(models).To(Equal([]shared.InsertionModel{test_data.CatFileVowModel()}))
+		Expect(models).To(Equal([]event.InsertionModel{test_data.CatFileVowModel()}))
 	})
 })

transformers/events/cat_file/vow/initializer/initializer.go
@@ -17,15 +17,15 @@
 package initializer
 
 import (
-	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
-	"github.com/makerdao/vulcanizedb/libraries/shared/transformer"
-
 	"github.com/makerdao/vdb-mcd-transformers/transformers/events/cat_file/vow"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
+	"github.com/makerdao/vulcanizedb/libraries/shared/transformer"
 )
 
-var EventTransformerInitializer transformer.EventTransformerInitializer = shared.EventTransformer{
+var EventTransformerInitializer transformer.EventTransformerInitializer = event.Transformer{
 	Config:     shared.GetEventTransformerConfig(constants.CatFileVowLabel, constants.CatFileVowSignature()),
-	Converter:  &vow.CatFileVowConverter{},
+	Converter:  &vow.Converter{},
 	Repository: &vow.CatFileVowRepository{},
-}.NewEventTransformer
+}.NewTransformer