7527ccca203ea4a63f33c193ac16cb7c5b8023f4
Rob Mulholand <rmulholand@8thlight.com>
2019-12-16 13:02:05 -0600

    Refactor spot storage transformer tests
    - use shared behaviors helpers
    - prefer random vals for test vars
    - minimize scope for test vars
    - use different names for errors sharing scope

--- transformers/component_tests/spot/transformer_test.go
+++ transformers/component_tests/spot/transformer_test.go
@@ -36,8 +36,6 @@ import (
 var _ = Describe("Executing the transformer", func() {
 	var (
 		db                = test_config.NewTestDB(test_config.NewTestNode())
-		err               error
-		ilkID             int64
 		storageKeysLookup = storage.NewKeysLookup(spot.NewKeysLoader(&mcdStorage.MakerStorageRepository{}))
 		repository        = spot.SpotStorageRepository{}
 		contractAddress   = "a57d4123c8a80ac410e924df9d5e47765ffd1375"
@@ -46,19 +44,14 @@ var _ = Describe("Executing the transformer", func() {
 			StorageKeysLookup: storageKeysLookup,
 			Repository:        &repository,
 		}
-		headerID int64
-		header   = fakes.FakeHeader
+		header = fakes.FakeHeader
 	)
 
 	BeforeEach(func() {
 		test_config.CleanTestDB(db)
 		transformer.NewTransformer(db)
-		ilk := "0x434f4c352d410000000000000000000000000000000000000000000000000000"
-		ilkID, err = shared.GetOrCreateIlk(ilk, db)
-		Expect(err).NotTo(HaveOccurred())
 		headerRepository := repositories.NewHeaderRepository(db)
-		var insertHeaderErr error
-		headerID, insertHeaderErr = headerRepository.CreateOrUpdateHeader(header)
+		headerID, insertHeaderErr := headerRepository.CreateOrUpdateHeader(header)
 		Expect(insertHeaderErr).NotTo(HaveOccurred())
 		header.Id = headerID
 	})
@@ -67,26 +60,28 @@ var _ = Describe("Executing the transformer", func() {
 		key := common.HexToHash("0000000000000000000000000000000000000000000000000000000000000002")
 		value := common.HexToHash("00000000000000000000000057aa8b02f5d3e28371fedcf672c8668869f9aac7")
 		spotVatDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
-		err := transformer.Execute(spotVatDiff)
-		Expect(err).NotTo(HaveOccurred())
 
+		executeErr := transformer.Execute(spotVatDiff)
+
+		Expect(executeErr).NotTo(HaveOccurred())
 		var vatResult test_helpers.VariableRes
-		err = db.Get(&vatResult, `SELECT diff_id, header_id, vat AS value FROM maker.spot_vat`)
-		Expect(err).NotTo(HaveOccurred())
-		test_helpers.AssertVariable(vatResult, spotVatDiff.ID, headerID, "0x57aA8B02F5D3E28371FEdCf672C8668869f9AAC7")
+		getErr := db.Get(&vatResult, `SELECT diff_id, header_id, vat AS value FROM maker.spot_vat`)
+		Expect(getErr).NotTo(HaveOccurred())
+		test_helpers.AssertVariable(vatResult, spotVatDiff.ID, header.Id, "0x57aA8B02F5D3E28371FEdCf672C8668869f9AAC7")
 	})
 
 	It("reads in a Spot Par storage diff row and persists it", func() {
 		key := common.HexToHash("0000000000000000000000000000000000000000000000000000000000000003")
 		value := common.HexToHash("0000000000000000000000000000000000000000033b2e3c9fd0803ce8000000")
 		spotParDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
-		err := transformer.Execute(spotParDiff)
-		Expect(err).NotTo(HaveOccurred())
 
+		executeErr := transformer.Execute(spotParDiff)
+
+		Expect(executeErr).NotTo(HaveOccurred())
 		var parResult test_helpers.VariableRes
-		err = db.Get(&parResult, `SELECT diff_id, header_id, par AS value FROM maker.spot_par`)
-		Expect(err).NotTo(HaveOccurred())
-		test_helpers.AssertVariable(parResult, spotParDiff.ID, headerID, "1000000000000000000000000000")
+		getErr := db.Get(&parResult, `SELECT diff_id, header_id, par AS value FROM maker.spot_par`)
+		Expect(getErr).NotTo(HaveOccurred())
+		test_helpers.AssertVariable(parResult, spotParDiff.ID, header.Id, "1000000000000000000000000000")
 	})
 
 	It("reads in a Spot Live storage diff row and persists it", func() {
@@ -94,39 +89,51 @@ var _ = Describe("Executing the transformer", func() {
 		value := common.HexToHash("0000000000000000000000000000000000000000000000000000000000000001")
 		spotLiveDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
 
-		err := transformer.Execute(spotLiveDiff)
-		Expect(err).NotTo(HaveOccurred())
+		executeErr := transformer.Execute(spotLiveDiff)
+
+		Expect(executeErr).NotTo(HaveOccurred())
 		var liveResult test_helpers.VariableRes
-		err = db.Get(&liveResult, `SELECT diff_id, header_id, live AS value FROM maker.spot_live`)
-		Expect(err).NotTo(HaveOccurred())
-		test_helpers.AssertVariable(liveResult, spotLiveDiff.ID, headerID, "1")
+		getErr := db.Get(&liveResult, `SELECT diff_id, header_id, live AS value FROM maker.spot_live`)
+		Expect(getErr).NotTo(HaveOccurred())
+		test_helpers.AssertVariable(liveResult, spotLiveDiff.ID, header.Id, "1")
 	})
 
-	It("reads in a Spot Ilk Pip storage diff row and persists it", func() {
-		key := common.HexToHash("1730ac98111482efebd8acadb14d7fa301298e0d95bf3c34c3378ef524670bc6")
-		value := common.HexToHash("000000000000000000000000a53e6efb4cbed841eace02220498860905e94998")
-		spotIlkPipDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
+	Describe("ilk", func() {
+		var ilkID int64
 
-		err := transformer.Execute(spotIlkPipDiff)
-		Expect(err).NotTo(HaveOccurred())
+		BeforeEach(func() {
+			ilk := "0x434f4c352d410000000000000000000000000000000000000000000000000000"
+			var ilkErr error
+			ilkID, ilkErr = shared.GetOrCreateIlk(ilk, db)
+			Expect(ilkErr).NotTo(HaveOccurred())
+		})
 
-		var ilkPipResult test_helpers.MappingRes
-		err = db.Get(&ilkPipResult, `SELECT diff_id, header_id, ilk_id AS key, pip AS value FROM maker.spot_ilk_pip`)
-		Expect(err).NotTo(HaveOccurred())
-		test_helpers.AssertMapping(ilkPipResult, spotIlkPipDiff.ID, headerID, strconv.FormatInt(ilkID, 10), "0xA53e6EFB4cBeD841Eace02220498860905E94998")
-	})
+		It("reads in a Spot Ilk Pip storage diff row and persists it", func() {
+			key := common.HexToHash("1730ac98111482efebd8acadb14d7fa301298e0d95bf3c34c3378ef524670bc6")
+			value := common.HexToHash("000000000000000000000000a53e6efb4cbed841eace02220498860905e94998")
+			spotIlkPipDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
 
-	It("reads in a Spot Ilk Mat storage diff row and persists it", func() {
-		key := common.HexToHash("1730ac98111482efebd8acadb14d7fa301298e0d95bf3c34c3378ef524670bc7")
-		value := common.HexToHash("000000000000000000000000000000000000000006765c793fa10079d0000000")
-		spotIlkMatDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
+			executeErr := transformer.Execute(spotIlkPipDiff)
 
-		err := transformer.Execute(spotIlkMatDiff)
-		Expect(err).NotTo(HaveOccurred())
+			Expect(executeErr).NotTo(HaveOccurred())
+			var ilkPipResult test_helpers.MappingRes
+			getErr := db.Get(&ilkPipResult, `SELECT diff_id, header_id, ilk_id AS key, pip AS value FROM maker.spot_ilk_pip`)
+			Expect(getErr).NotTo(HaveOccurred())
+			test_helpers.AssertMapping(ilkPipResult, spotIlkPipDiff.ID, header.Id, strconv.FormatInt(ilkID, 10), "0xA53e6EFB4cBeD841Eace02220498860905E94998")
+		})
 
-		var ilkRhoResult test_helpers.MappingRes
-		err = db.Get(&ilkRhoResult, `SELECT diff_id, header_id, ilk_id AS key, mat AS value FROM maker.spot_ilk_mat`)
-		Expect(err).NotTo(HaveOccurred())
-		test_helpers.AssertMapping(ilkRhoResult, spotIlkMatDiff.ID, headerID, strconv.FormatInt(ilkID, 10), "2000000000000000000000000000")
+		It("reads in a Spot Ilk Mat storage diff row and persists it", func() {
+			key := common.HexToHash("1730ac98111482efebd8acadb14d7fa301298e0d95bf3c34c3378ef524670bc7")
+			value := common.HexToHash("000000000000000000000000000000000000000006765c793fa10079d0000000")
+			spotIlkMatDiff := test_helpers.CreateDiffRecord(db, header, transformer.HashedAddress, key, value)
+
+			executeErr := transformer.Execute(spotIlkMatDiff)
+
+			Expect(executeErr).NotTo(HaveOccurred())
+			var ilkRhoResult test_helpers.MappingRes
+			getErr := db.Get(&ilkRhoResult, `SELECT diff_id, header_id, ilk_id AS key, mat AS value FROM maker.spot_ilk_mat`)
+			Expect(getErr).NotTo(HaveOccurred())
+			test_helpers.AssertMapping(ilkRhoResult, spotIlkMatDiff.ID, header.Id, strconv.FormatInt(ilkID, 10), "2000000000000000000000000000")
+		})
 	})
 })
