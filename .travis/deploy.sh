ea69a2ee794700e0f7f69249671c256d54079f7e
daimesava <daimesava@users.noreply.github.com>
2020-02-12 21:06:06 +0200

    dockerhub via travis

---
+++ .travis/deploy.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+set -e
+
+function message() {
+    echo
+    echo -----------------------------------
+    echo "$@"
+    echo -----------------------------------
+    echo
+}
+
+ENVIRONMENT=$1
+if [ "$ENVIRONMENT" == "prod" ]; then
+TAG=latest
+elif [ "$ENVIRONMENT" == "staging" ]; then
+TAG=staging
+else
+   message UNKNOWN ENVIRONMENT
+fi
+
+if [ -z "$ENVIRONMENT" ]; then
+    echo 'You must specifiy an envionrment (bash deploy.sh <ENVIRONMENT>).'
+    echo 'Allowed values are "staging" or "prod"'
+    exit 1
+fi
+
+message BUILDING DOCKER IMAGE
+docker build -t makerdao/vdb-execute:$TAG .
+
+message LOGGING INTO DOCKERHUB
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
+
+message PUSHING DOCKER IMAGE
+docker push makerdao/vdb-execute:$TAG .
+
+# message DEPLOYING SERVICE
