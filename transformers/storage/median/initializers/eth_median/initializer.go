259df61c2dc1312469240579ca8b86916268dc82
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2020-03-31 14:58:12 -0700

    Create initializers for each ilk type

---
+++ transformers/storage/median/initializers/eth_median/initializer.go
@@ -0,0 +1,8 @@
+package eth_median
+
+import (
+	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
+)
+
+var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_ETH"))
