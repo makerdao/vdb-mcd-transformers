b058c580047efcc1fcc3b16d266b145eb6901bd2
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2020-03-31 15:07:16 -0700

    Update config files and plugin exporter

--- plugins/transformerExporter.go
+++ plugins/transformerExporter.go
@@ -66,6 +66,8 @@ import (
 	usdc_flip "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/usdc_flip"
 	flop_storage "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flop/initializer"
 	jug "github.com/makerdao/vdb-mcd-transformers/transformers/storage/jug/initializer"
+	bat_median "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers/bat_median"
+	eth_median "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers/eth_median"
 	pot "github.com/makerdao/vdb-mcd-transformers/transformers/storage/pot/initializer"
 	spot "github.com/makerdao/vdb-mcd-transformers/transformers/storage/spot/initializer"
 	vat "github.com/makerdao/vdb-mcd-transformers/transformers/storage/vat/initializer"
@@ -138,9 +140,11 @@ func (e exporter) Export() ([]event.TransformerInitializer, []storage.Transforme
 		},
 		[]storage.TransformerInitializer{
 			bat_flip.StorageTransformerInitializer,
+			bat_median.StorageTransformerInitializer,
 			cat.StorageTransformerInitializer,
 			cdp_manager.StorageTransformerInitializer,
 			eth_flip_a.StorageTransformerInitializer,
+			eth_median.StorageTransformerInitializer,
 			flap_storage.StorageTransformerInitializer,
 			flop_storage.StorageTransformerInitializer,
 			jug.StorageTransformerInitializer,
