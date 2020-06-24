dd553e680d52abc972ccd99443af74c59baff1da
Sam MacPherson <xylom44@gmail.com>
2020-06-24 14:39:28 -0400

    added bite totals; added start and end totals for the flip/flap/flop auctions; use the start timestamp for the bucket to line up totals with the bites

--- db/migrations/20200615121551_create_time_auction_totals_query.sql
+++ db/migrations/20200615121551_create_time_auction_totals_query.sql
@@ -4,8 +4,10 @@ CREATE TYPE api.time_bid_total AS
     bucket_start TIMESTAMP,
     bucket_end TIMESTAMP,
     bucket_interval INTERVAL,
-    lot NUMERIC,
-    bid_amount NUMERIC
+    lot_start NUMERIC,
+    lot_end NUMERIC,
+    bid_amount_start NUMERIC,
+    bid_amount_end NUMERIC
 );
 
 CREATE FUNCTION api.time_flip_bid_totals(ilk_identifier TEXT, range_start TIMESTAMP, range_end TIMESTAMP, bucket_interval INTERVAL DEFAULT '1 day'::INTERVAL)
@@ -14,9 +16,11 @@ $$
 WITH buckets AS (SELECT generate_series(range_start, range_end - bucket_interval, bucket_interval) AS bucket_start),
 bid_results AS (
     SELECT bid_id,
-        MAX(block_timestamp) AS block_timestamp,
-        MIN(lot) AS lot,
-        MAX(bid_amount) AS bid_amount
+        MIN(block_timestamp) AS block_timestamp,
+        MAX(lot) AS lot_start,
+        MIN(lot) AS lot_end,
+        MIN(bid_amount) AS bid_amount_start,
+        MAX(bid_amount) AS bid_amount_end
     FROM maker.bid_event
     LEFT JOIN public.headers ON (headers.block_number = block_height)
     WHERE ilk_identifier = time_flip_bid_totals.ilk_identifier
@@ -25,8 +29,10 @@ bid_results AS (
 SELECT buckets.bucket_start AS bucket_start,
        buckets.bucket_start + bucket_interval AS bucket_end,
        bucket_interval AS bucket_interval,
-       COALESCE(SUM(lot), 0) AS lot,
-       COALESCE(SUM(bid_amount), 0) AS bid_amount
+       COALESCE(SUM(lot_start), 0) AS lot_start,
+       COALESCE(SUM(lot_end), 0) AS lot_end,
+       COALESCE(SUM(bid_amount_start), 0) AS bid_amount_start,
+       COALESCE(SUM(bid_amount_end), 0) AS bid_amount_end
 FROM buckets
     LEFT JOIN bid_results ON (
         block_timestamp >= extract(epoch FROM buckets.bucket_start) AND
@@ -51,9 +57,11 @@ flap_address AS (
 ),
 bid_results AS (
     SELECT bid_id,
-        MAX(block_timestamp) AS block_timestamp,
-        MIN(lot) AS lot,
-        MAX(bid_amount) AS bid_amount
+        MIN(block_timestamp) AS block_timestamp,
+        MAX(lot) AS lot_start,
+        MIN(lot) AS lot_end,
+        MIN(bid_amount) AS bid_amount_start,
+        MAX(bid_amount) AS bid_amount_end
     FROM maker.bid_event
     LEFT JOIN public.headers ON (headers.block_number = block_height)
     WHERE contract_address = (SELECT * FROM flap_address)
@@ -62,8 +70,10 @@ bid_results AS (
 SELECT buckets.bucket_start AS bucket_start,
        buckets.bucket_start + bucket_interval AS bucket_end,
        bucket_interval AS bucket_interval,
-       COALESCE(SUM(lot), 0) AS lot,
-       COALESCE(SUM(bid_amount), 0) AS bid_amount
+       COALESCE(SUM(lot_start), 0) AS lot_start,
+       COALESCE(SUM(lot_end), 0) AS lot_end,
+       COALESCE(SUM(bid_amount_start), 0) AS bid_amount_start,
+       COALESCE(SUM(bid_amount_end), 0) AS bid_amount_end
 FROM buckets
     LEFT JOIN bid_results ON (
         block_timestamp >= extract(epoch FROM buckets.bucket_start) AND
@@ -88,9 +98,11 @@ flop_address AS (
 ),
 bid_results AS (
     SELECT bid_id,
-        MAX(block_timestamp) AS block_timestamp,
-        MIN(lot) AS lot,
-        MAX(bid_amount) AS bid_amount
+        MIN(block_timestamp) AS block_timestamp,
+        MAX(lot) AS lot_start,
+        MIN(lot) AS lot_end,
+        MIN(bid_amount) AS bid_amount_start,
+        MAX(bid_amount) AS bid_amount_end
     FROM maker.bid_event
     LEFT JOIN public.headers ON (headers.block_number = block_height)
     WHERE contract_address = (SELECT * FROM flop_address)
@@ -99,10 +111,52 @@ bid_results AS (
 SELECT buckets.bucket_start AS bucket_start,
        buckets.bucket_start + bucket_interval AS bucket_end,
        bucket_interval AS bucket_interval,
-       COALESCE(SUM(lot), 0) AS lot,
-       COALESCE(SUM(bid_amount), 0) AS bid_amount
+       COALESCE(SUM(lot_start), 0) AS lot_start,
+       COALESCE(SUM(lot_end), 0) AS lot_end,
+       COALESCE(SUM(bid_amount_start), 0) AS bid_amount_start,
+       COALESCE(SUM(bid_amount_end), 0) AS bid_amount_end
 FROM buckets
     LEFT JOIN bid_results ON (
+        block_timestamp >= extract(epoch FROM buckets.bucket_start) AND
+        block_timestamp < extract(epoch FROM buckets.bucket_start + bucket_interval)
+    )
+GROUP BY bucket_start, bucket_end, bucket_interval
+ORDER BY bucket_start
+$$
+    LANGUAGE sql
+    STRICT
+    STABLE;
+
+CREATE TYPE api.time_bite_total AS
+(
+    bucket_start TIMESTAMP,
+    bucket_end TIMESTAMP,
+    bucket_interval INTERVAL,
+    ink NUMERIC,
+    art NUMERIC,
+    tab NUMERIC
+);
+
+CREATE FUNCTION api.time_bite_totals(ilk_identifier TEXT, range_start TIMESTAMP, range_end TIMESTAMP, bucket_interval INTERVAL DEFAULT '1 day'::INTERVAL)
+    RETURNS SETOF api.time_bite_total AS
+$$
+WITH buckets AS (SELECT generate_series(range_start, range_end - bucket_interval, bucket_interval) AS bucket_start),
+bite_results AS (
+    SELECT *
+    FROM maker.bite
+    LEFT JOIN public.headers ON (headers.id = bite.header_id)
+    LEFT JOIN maker.urns ON (urns.id = bite.urn_id)
+    LEFT JOIN maker.ilks ON (ilks.id = urns.ilk_id)
+    WHERE ilks.identifier = time_bite_totals.ilk_identifier
+)
+SELECT buckets.bucket_start AS bucket_start,
+       buckets.bucket_start + bucket_interval AS bucket_end,
+       bucket_interval AS bucket_interval,
+       COALESCE(SUM(ink), 0) AS ink,
+       COALESCE(SUM(art), 0) AS art,
+       COALESCE(SUM(tab), 0) AS tab
+FROM buckets
+    LEFT JOIN bite_results ON (
         block_timestamp >= extract(epoch FROM buckets.bucket_start) AND
         block_timestamp < extract(epoch FROM buckets.bucket_start + bucket_interval)
     )
@@ -118,3 +172,5 @@ DROP FUNCTION api.time_flip_bid_totals(TEXT, TIMESTAMP, TIMESTAMP, INTERVAL);
 DROP FUNCTION api.time_flap_bid_totals(TIMESTAMP, TIMESTAMP, INTERVAL);
 DROP FUNCTION api.time_flop_bid_totals(TIMESTAMP, TIMESTAMP, INTERVAL);
 DROP TYPE api.time_bid_total CASCADE;
+DROP FUNCTION api.time_bite_totals(TEXT, TIMESTAMP, TIMESTAMP, INTERVAL);
+DROP TYPE api.time_bite_total CASCADE;
