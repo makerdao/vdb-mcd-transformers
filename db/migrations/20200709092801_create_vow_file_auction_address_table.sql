31f145be95787f314350f73c23c118781f2afe2d
Elizabeth Engelman <emengelman@gmail.com>
2020-07-09 11:14:38 -0500

    Add vow_file_auction_address table

---
+++ db/migrations/20200709092801_create_vow_file_auction_address_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+CREATE TABLE maker.vow_file_auction_address
+(
+    id        SERIAL PRIMARY KEY,
+    header_id INTEGER NOT NULL REFERENCES public.headers (id) ON DELETE CASCADE,
+    log_id    BIGINT  NOT NULL REFERENCES public.event_logs (id) ON DELETE CASCADE,
+    what      TEXT,
+    data      INTEGER NOT NULL REFERENCES public.addresses (id) ON DELETE CASCADE,
+    UNIQUE (header_id, log_id)
+);
+
+CREATE INDEX vow_file_auction_address_header_index
+    ON maker.vow_file_auction_address (header_id);
+CREATE INDEX vvow_file_auction_address_log_index
+    ON maker.vow_file_auction_address (log_id);
+CREATE INDEX vow_file_auction_address_data_index
+    ON maker.vow_file_auction_address (data);
+
+-- +goose Down
+DROP TABLE maker.vow_file_auction_address;
