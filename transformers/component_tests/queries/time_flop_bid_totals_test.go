d44ee830165b06e8e58f6f8488db112d4058831c
Sam MacPherson <xylom44@gmail.com>
2020-07-12 13:42:04 -0400

    typo fix on flaps test; added flop bid totals test

--- transformers/component_tests/queries/time_flop_bid_totals_test.go
+++ transformers/component_tests/queries/time_flop_bid_totals_test.go
@@ -19,7 +19,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Time Flap Bid Totals query", func() {
+var _ = Describe("Time Flop Bid Totals query", func() {
 	var (
 		headerRepo      datastore.HeaderRepository
 		contractAddress = fakes.FakeAddress.Hex()
@@ -28,7 +28,7 @@ var _ = Describe("Time Flap Bid Totals query", func() {
 		bid, lot        int
 		headerOne       core.Header
 		fakeBidId       int
-		flapKickEvent   event.InsertionModel
+		flopKickEvent   event.InsertionModel
 	)
 
 	BeforeEach(func() {
@@ -41,24 +41,25 @@ var _ = Describe("Time Flap Bid Totals query", func() {
 		blockOne = rand.Int()
 		timestampOne = int64(rand.Int31())
 		headerOne = createHeader(blockOne, int(timestampOne), headerRepo)
-		flapKickLog := test_data.CreateTestLog(headerOne.Id, db)
+		flopKickLog := test_data.CreateTestLog(headerOne.Id, db)
 		addressId, addressErr := shared.GetOrCreateAddress(contractAddress, db)
 		Expect(addressErr).NotTo(HaveOccurred())
 
-		flapKickEvent = test_data.FlapKickModel()
-		flapKickEvent.ColumnValues[event.HeaderFK] = headerOne.Id
-		flapKickEvent.ColumnValues[event.LogFK] = flapKickLog.ID
-		flapKickEvent.ColumnValues[event.AddressFK] = addressId
-		flapKickEvent.ColumnValues[constants.BidIDColumn] = strconv.Itoa(fakeBidId)
-		flapKickEvent.ColumnValues[constants.BidColumn] = strconv.Itoa(0)
-		flapKickEvent.ColumnValues[constants.LotColumn] = strconv.Itoa(lot)
-		flapKickErr := event.PersistModels([]event.InsertionModel{flapKickEvent}, db)
-		Expect(flapKickErr).NotTo(HaveOccurred())
+		flopKickEvent = test_data.FlopKickModel()
+		flopKickEvent.ColumnValues[event.HeaderFK] = headerOne.Id
+		flopKickEvent.ColumnValues[event.LogFK] = flopKickLog.ID
+		flopKickEvent.ColumnValues[event.AddressFK] = addressId
+		flopKickEvent.ColumnValues[constants.BidIDColumn] = strconv.Itoa(fakeBidId)
+		flopKickEvent.ColumnValues[constants.BidColumn] = strconv.Itoa(bid)
+		flopKickEvent.ColumnValues[constants.LotColumn] = strconv.Itoa(lot)
+		flopKickErr := event.PersistModels([]event.InsertionModel{flopKickEvent}, db)
+		Expect(flopKickErr).NotTo(HaveOccurred())
 	})
 
 	Context("when called with an hourly 2 hour range with the range start on the first block", func() {
 		It("returns the all the bid results under the first hour and 0 for the second hour", func() {
-			tendLog := test_data.CreateTestLog(headerOne.Id, db)
+			dentLog := test_data.CreateTestLog(headerOne.Id, db)
+			dentLot := lot / (rand.Int()%8 + 2)
 
 			oneHour := timestampOne + 3600
 			twoHours := timestampOne + 7200
@@ -67,43 +68,33 @@ var _ = Describe("Time Flap Bid Totals query", func() {
 			dateMiddle := time.Unix(oneHour, 0).UTC().Format(time.RFC3339)
 			dateEnd := time.Unix(twoHours, 0).UTC().Format(time.RFC3339)
 
-			flapTendErr := test_helpers.CreateTend(test_helpers.TendCreationInput{
-				DB:              db,
-				ContractAddress: contractAddress,
-				BidId:           fakeBidId,
-				Lot:             lot,
-				BidAmount:       bid,
-				TendHeaderId:    headerOne.Id,
-				TendLogId:       tendLog.ID,
-			})
-			Expect(flapTendErr).NotTo(HaveOccurred())
-
 			headerTwo := createHeader(blockOne+1, int(twoHours), headerRepo)
 
-			tickLog := test_data.CreateTestLog(headerOne.Id, db)
-			flapTickErr := test_helpers.CreateTick(test_helpers.TickCreationInput{
+			flopDentErr := test_helpers.CreateDent(test_helpers.DentCreationInput{
 				DB:              db,
-				BidId:           fakeBidId,
 				ContractAddress: contractAddress,
-				TickHeaderId:    headerTwo.Id,
-				TickLogId:       tickLog.ID,
+				BidId:           fakeBidId,
+				Lot:             dentLot,
+				BidAmount:       bid,
+				DentHeaderId:    headerTwo.Id,
+				DentLogId:       dentLog.ID,
 			})
-			Expect(flapTickErr).NotTo(HaveOccurred())
+			Expect(flopDentErr).NotTo(HaveOccurred())
 
-			flapDealErr := test_helpers.CreateDeal(test_helpers.DealCreationInput{
+			flopDealErr := test_helpers.CreateDeal(test_helpers.DealCreationInput{
 				DB:              db,
 				BidId:           fakeBidId,
 				ContractAddress: contractAddress,
 				DealHeaderId:    headerTwo.Id,
 			})
-			Expect(flapDealErr).NotTo(HaveOccurred())
+			Expect(flopDealErr).NotTo(HaveOccurred())
 
 			var actualBidTotals []test_helpers.BucketedBidTotals
-			queryErr := db.Select(&actualBidTotals, `SELECT bucket_start, bucket_end, bucket_interval, lot_start, lot_end, bid_amount_start, bid_amount_end FROM api.time_flap_bid_totals($1, $2, '1 hour'::INTERVAL)`, dateStart, dateEnd)
+			queryErr := db.Select(&actualBidTotals, `SELECT bucket_start, bucket_end, bucket_interval, lot_start, lot_end, bid_amount_start, bid_amount_end FROM api.time_flop_bid_totals($1, $2, '1 hour'::INTERVAL)`, dateStart, dateEnd)
 			Expect(queryErr).NotTo(HaveOccurred())
 
 			Expect(actualBidTotals).To(ConsistOf(
-				test_helpers.BucketedBidTotals{BucketStart: dateStart, BucketEnd: dateMiddle, BucketInterval: "01:00:00", LotStart: flapKickEvent.ColumnValues["lot"].(string), LotEnd: strconv.Itoa(lot), BidAmountStart: flapKickEvent.ColumnValues["bid"].(string), BidAmountEnd: strconv.Itoa(bid)},
+				test_helpers.BucketedBidTotals{BucketStart: dateStart, BucketEnd: dateMiddle, BucketInterval: "01:00:00", LotStart: flopKickEvent.ColumnValues["lot"].(string), LotEnd: strconv.Itoa(dentLot), BidAmountStart: flopKickEvent.ColumnValues["bid"].(string), BidAmountEnd: strconv.Itoa(bid)},
 				test_helpers.BucketedBidTotals{BucketStart: dateMiddle, BucketEnd: dateEnd, BucketInterval: "01:00:00", LotStart: "0", LotEnd: "0", BidAmountStart: "0", BidAmountEnd: "0"}))
 		})
 	})
