8fde4a6a33e3940e2db44f4928e245cd1ee12838
Elizabeth Engelman <emengelman@gmail.com>
2020-08-26 16:28:15 -0500

    Use updated execute cmd to transform new & unrecognized diffs separately

--- dockerfiles/execute/startup_script.sh
+++ dockerfiles/execute/startup_script.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 source run_migrations.sh
 
-DIFF_STARTING_BLOCK="-1"
+NEW_DIFF_STARTING_BLOCK="-1"
+UNRECOGNIZED_DIFF_STARTING_BLOCK="-1"
 
-if test -n "$DIFF_BLOCK_FROM_HEAD_OF_CHAIN"; then
-    DIFF_STARTING_BLOCK=$DIFF_BLOCK_FROM_HEAD_OF_CHAIN
+if test -n "$NEW_DIFF_BLOCK_FROM_HEAD_OF_CHAIN"; then
+    NEW_DIFF_STARTING_BLOCK=$NEW_DIFF_BLOCK_FROM_HEAD_OF_CHAIN
 fi
 
+if test -n "$UNRECOGNIZED_DIFF_BLOCK_FROM_HEAD_OF_CHAIN"; then
+    UNRECOGNIZED_DIFF_STARTING_BLOCK=$UNRECOGNIZED_DIFF_BLOCK_FROM_HEAD_OF_CHAIN
+fi
 echo "Starting transformer execution..."
 # Fire up execute
-./vulcanizedb execute --config config.toml -d $DIFF_STARTING_BLOCK
+./vulcanizedb execute --config config.toml -d $NEW_DIFF_STARTING_BLOCK -u $UNRECOGNIZED_DIFF_STARTING_BLOCK
