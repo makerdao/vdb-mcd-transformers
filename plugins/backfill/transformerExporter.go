088f00ec0a1755c677fed28988f7bf36ccb69e7e
Elizabeth Engelman <emengelman@gmail.com>
2020-08-31 16:19:16 -0500

    Configure execute and backfill processes to include cat_file_box events

--- plugins/backfill/transformerExporter.go
+++ plugins/backfill/transformerExporter.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	cat_file_box "github.com/makerdao/vdb-mcd-transformers/transformers/events/cat_file/box/initializer"
 	event "github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	storage "github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
@@ -13,7 +14,9 @@ type exporter string
 var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
-	return []event.TransformerInitializer{},
+	return []event.TransformerInitializer{
+		cat_file_box.EventTransformerInitializer,
+	},
 		[]storage.TransformerInitializer{},
 		[]interface1.ContractTransformerInitializer{}
 }
