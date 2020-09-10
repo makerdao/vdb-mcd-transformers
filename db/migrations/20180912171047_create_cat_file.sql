bbf5b50a7548ba0c8322b7e21a2e0b93f4398774
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2020-09-10 12:31:13 -0700

    Remove changes to existing migration

--- db/migrations/20180912171047_create_cat_file.sql
+++ db/migrations/20180912171047_create_cat_file.sql
@@ -1,23 +1,21 @@
 -- +goose Up
-CREATE TABLE maker.cat_file_chop_lump_dunk
+CREATE TABLE maker.cat_file_chop_lump
 (
-    id         SERIAL PRIMARY KEY,
-    header_id  INTEGER NOT NULL REFERENCES public.headers (id) ON DELETE CASCADE,
-    address_id BIGINT  NOT NULL REFERENCES public.addresses (id) ON DELETE CASCADE,
-    msg_sender BIGINT  NOT NULL REFERENCES public.addresses (id) ON DELETE CASCADE,
-    log_id     BIGINT  NOT NULL REFERENCES public.event_logs (id) ON DELETE CASCADE,
-    ilk_id     INTEGER NOT NULL REFERENCES maker.ilks (id) ON DELETE CASCADE,
-    what       TEXT,
-    data       NUMERIC,
+    id        SERIAL PRIMARY KEY,
+    header_id INTEGER NOT NULL REFERENCES public.headers (id) ON DELETE CASCADE,
+    log_id    BIGINT  NOT NULL REFERENCES public.event_logs (id) ON DELETE CASCADE,
+    ilk_id    INTEGER NOT NULL REFERENCES maker.ilks (id) ON DELETE CASCADE,
+    what      TEXT,
+    data      NUMERIC,
     UNIQUE (header_id, log_id)
 );
 
-CREATE INDEX cat_file_chop_lump_dunk_header_index
-    ON maker.cat_file_chop_lump_dunk (header_id);
-CREATE INDEX cat_file_chop_lump_dunk_log_index
-    ON maker.cat_file_chop_lump_dunk (log_id);
-CREATE INDEX cat_file_chop_lump_dunk_ilk_index
-    ON maker.cat_file_chop_lump_dunk (ilk_id);
+CREATE INDEX cat_file_chop_lump_header_index
+    ON maker.cat_file_chop_lump (header_id);
+CREATE INDEX cat_file_chop_lump_log_index
+    ON maker.cat_file_chop_lump (log_id);
+CREATE INDEX cat_file_chop_lump_ilk_index
+    ON maker.cat_file_chop_lump (ilk_id);
 
 CREATE TABLE maker.cat_file_flip
 (
@@ -54,6 +52,15 @@ CREATE INDEX cat_file_vow_log_index
 
 
 -- +goose Down
-DROP TABLE maker.cat_file_chop_lump_dunk;
+DROP INDEX maker.cat_file_chop_lump_header_index;
+DROP INDEX maker.cat_file_chop_lump_log_index;
+DROP INDEX maker.cat_file_chop_lump_ilk_index;
+DROP INDEX maker.cat_file_flip_header_index;
+DROP INDEX maker.cat_file_flip_log_index;
+DROP INDEX maker.cat_file_flip_ilk_index;
+DROP INDEX maker.cat_file_vow_header_index;
+DROP INDEX maker.cat_file_vow_log_index;
+
+DROP TABLE maker.cat_file_chop_lump;
 DROP TABLE maker.cat_file_flip;
 DROP TABLE maker.cat_file_vow;
