85016d6414cc729e47cbfbe75fc978ddfd08217c
Elizabeth <eengelman@8thlight.com>
2020-09-17 11:49:31 -0500

    Add liq 1.2, PAXUSD and USDT contracts for backfill events process (#310)
    * Add liq 1.2, PAXUSD and USDT contracts for backfill events process
    
    * Set backfill events command ending block number

--- dockerfiles/backfill_events/startup_script.sh
+++ dockerfiles/backfill_events/startup_script.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-if test -z "$ENDING_BLOCK_NUMBER"
-then
-    echo ENDING_BLOCK_NUMBER is required and no value was given
-    exit 1
-fi
-echo "Starting event backfill from block $ENDING_BLOCK_NUMBER"
+echo "Starting event backfill from block 10875761"
 # Fire up execute
-./vulcanizedb backfillEvents --config config.toml -e $ENDING_BLOCK_NUMBER
+./vulcanizedb backfillEvents --config config.toml -e 10875761
