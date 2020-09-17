12744816f2269a8192a0bd98d234b99bbf6bc405
Elizabeth <eengelman@8thlight.com>
2020-09-17 13:33:49 -0500

    Vdb 1635 update backfill events (#314)
    * Add event initializers to backfill transformerExporter
    
    * Update log in backfill events startup script

--- dockerfiles/backfill_events/startup_script.sh
+++ dockerfiles/backfill_events/startup_script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "Starting event backfill from block 10875761"
+echo "Starting event backfill to block 10875761"
 # Fire up execute
 ./vulcanizedb backfillEvents --config config.toml -e 10875761
