e0ef4b9caaeda5be47b07e5a84bb60947f052058
daimesava <daimesava@noreply.users.github.com>
2020-09-24 20:53:59 +0300

    rename execute repo and service for production env

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,8 +13,10 @@ function message() {
 ENVIRONMENT=$1
 if [ "$ENVIRONMENT" == "prod" ]; then
 TAG=latest
+EXECUTE_NAME=vdb-mcd-execute
 elif [ "$ENVIRONMENT" == "staging" ]; then
 TAG=staging
+EXECUTE_NAME=vdb-execute
 else
    message UNKNOWN ENVIRONMENT
 fi
@@ -26,7 +28,7 @@ if [ -z "$ENVIRONMENT" ]; then
 fi
 
 message BUILDING EXECUTE DOCKER IMAGE
-docker build -f dockerfiles/execute/Dockerfile . -t makerdao/vdb-execute:$TAG
+docker build -f dockerfiles/execute/Dockerfile . -t makerdao/$EXECUTE_NAME:$TAG
 
 message BUILDING EXTRACT-DIFFS DOCKER IMAGE
 docker build -f dockerfiles/extract_diffs/Dockerfile . -t makerdao/vdb-extract-diffs:$TAG
@@ -41,7 +43,7 @@ message LOGGING INTO DOCKERHUB
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
 
 message PUSHING EXECUTE DOCKER IMAGE
-docker push makerdao/vdb-execute:$TAG
+docker push makerdao/$EXECUTE_NAME:$TAG
 
 message PUSHING EXTRACT-DIFFS DOCKER IMAGE
 docker push makerdao/vdb-extract-diffs:$TAG
@@ -55,7 +57,7 @@ docker push makerdao/vdb-backfill-events:$TAG
 # service deploy
 if [ "$ENVIRONMENT" == "prod" ]; then
   message DEPLOYING EXECUTE
-  aws ecs update-service --cluster vdb-cluster-$ENVIRONMENT --service vdb-execute-$ENVIRONMENT --force-new-deployment --endpoint https://ecs.$PROD_REGION.amazonaws.com --region $PROD_REGION
+  aws ecs update-service --cluster vdb-cluster-$ENVIRONMENT --service vdb-mcd-execute-$ENVIRONMENT --force-new-deployment --endpoint https://ecs.$PROD_REGION.amazonaws.com --region $PROD_REGION
 
    message DEPLOYING EXTRACT-DIFFS
   aws ecs update-service --cluster vdb-cluster-$ENVIRONMENT --service vdb-extract-diffs-$ENVIRONMENT --force-new-deployment --endpoint https://ecs.$PROD_REGION.amazonaws.com --region $PROD_REGION
