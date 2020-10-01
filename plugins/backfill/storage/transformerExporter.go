be220b996c602b34ebe059e8b5cbea8a2bfe75ce
Elizabeth <eengelman@8thlight.com>
2020-10-01 17:16:15 -0500

    Vdb backfill storage (#329)
    * Configure backfill storage for COMP, LRC and LINK
    
    * Update backfill storage startup script for new collaterals

--- plugins/backfill/storage/transformerExporter.go
+++ plugins/backfill/storage/transformerExporter.go
@@ -3,8 +3,14 @@
 package main
 
 import (
-	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
-	"github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
+	flip_comp_a_v1_1_2 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/comp_a/v1_1_2"
+	flip_link_a_v1_1_2 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/link_a/v1_1_2"
+	flip_lrc_a_v1_1_2 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/lrc_a/v1_1_2"
+	median_comp "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers/median_comp"
+	median_link "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers/median_link"
+	median_lrc "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers/median_lrc"
+	event "github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
+	storage "github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
 )
 
@@ -14,6 +20,13 @@ var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
 	return []event.TransformerInitializer{},
-		[]storage.TransformerInitializer{},
-		[]interface1.ContractTransformerInitializer{}
+		   []storage.TransformerInitializer{
+				flip_comp_a_v1_1_2.StorageTransformerInitializer,
+				flip_link_a_v1_1_2.StorageTransformerInitializer,
+				flip_lrc_a_v1_1_2.StorageTransformerInitializer,
+				median_comp.StorageTransformerInitializer,
+				median_link.StorageTransformerInitializer,
+				median_lrc.StorageTransformerInitializer,
+		   },
+		   []interface1.ContractTransformerInitializer{}
 }
