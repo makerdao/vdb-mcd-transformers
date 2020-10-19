b8b358270ee9083fd5a220e1719d4e48bf063c2f
Elizabeth Engelman <emengelman@gmail.com>
2020-10-19 14:02:56 -0500

    Omit adding new Median and OSM contracts when necessary
    This is required for a new collateral like ETH-B where the median and
    osm contracts are the same as ETH-A.

--- cmd/newCollateralGenerator.go
+++ cmd/newCollateralGenerator.go
@@ -8,17 +8,19 @@ import (
 )
 
 var (
-	collateralName    string
-	collateralVersion string
-	flipAddress       string
-	flipAbi           string
-	flipBlock         int
-	medianAddress     string
-	medianAbi         string
-	medianBlock       int
-	osmAddress        string
-	osmAbi            string
-	osmBlock          int
+	collateralName         string
+	collateralVersion      string
+	flipAddress            string
+	flipAbi                string
+	flipBlock              int
+	medianContractRequired bool
+	medianAddress          string
+	medianAbi              string
+	medianBlock            int
+	osmContractRequired    bool
+	osmAddress             string
+	osmAbi                 string
+	osmBlock               int
 )
 
 var addNewCollateralCmd = &cobra.Command{
@@ -44,9 +46,13 @@ func init() {
 	addNewCollateralCmd.Flags().StringVar(&flipAddress, "flip-address", "", "new collateral's flip contract address")
 	addNewCollateralCmd.Flags().StringVar(&flipAbi, "flip-abi", "", "new collateral's flip abi")
 	addNewCollateralCmd.Flags().IntVar(&flipBlock, "flip-block", 0, "new collateral's flip deployed block")
+
+	addNewCollateralCmd.Flags().BoolVar(&medianContractRequired, "median-contract-required", false, "pass this flag in when a median contract is required")
 	addNewCollateralCmd.Flags().StringVar(&medianAddress, "median-address", "", "new collateral's median contract address")
 	addNewCollateralCmd.Flags().StringVar(&medianAbi, "median-abi", "", "new collateral's median abi")
 	addNewCollateralCmd.Flags().IntVar(&medianBlock, "median-block", 0, "new collateral's median deployed block")
+
+	addNewCollateralCmd.Flags().BoolVar(&osmContractRequired, "osm-contract-required", false, "pass this flag in when a median contract is required")
 	addNewCollateralCmd.Flags().StringVar(&osmAddress, "osm-address", "", "new collateral's osm contract address")
 	addNewCollateralCmd.Flags().StringVar(&osmAbi, "osm-abi", "", "new collateral's osm abi")
 	addNewCollateralCmd.Flags().IntVar(&osmBlock, "osm-block", 0, "new collateral's osm deployed block")
@@ -76,10 +82,10 @@ func addNewCollateral() error {
 	contracts["osm"] = osmContract
 
 	configParser := config.Parser{}
-	configUpdater := config.NewConfigUpdater(collateral, contracts)
+	configUpdater := config.NewConfigUpdater(collateral, contracts, medianContractRequired, osmContractRequired)
 	newCollateralGenerator := generator.NewCollateralGenerator{
 		ConfigFileName: "mcdTransformers",
-		ConfigFilePath: "/Users/elizabethengelman/go/src/github.com/makerdao/vdb-mcd-transformers/environments/",
+		ConfigFilePath: "../environments/",
 		ConfigParser:   configParser,
 		ConfigUpdater:  configUpdater,
 	}
