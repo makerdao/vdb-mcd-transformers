0725d8553a948be1efad26f4afa1597f3a04e8f1
Elizabeth Engelman <emengelman@gmail.com>
2020-10-28 14:17:47 -0500

    Use a different library for cli prompts

--- cmd/newCollateralGenerator.go
+++ cmd/newCollateralGenerator.go
@@ -1,24 +1,20 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/makerdao/vdb-mcd-transformers/generators/new_collateral"
 	"github.com/makerdao/vdb-mcd-transformers/generators/new_collateral/config"
 	"github.com/makerdao/vdb-mcd-transformers/generators/new_collateral/helpers"
 	"github.com/makerdao/vdb-mcd-transformers/generators/new_collateral/initializer"
+	"github.com/makerdao/vdb-mcd-transformers/generators/new_collateral/prompts"
 	"github.com/makerdao/vdb-mcd-transformers/generators/new_collateral/types"
-	"github.com/manifoldco/promptui"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var (
-	collateral             types.Collateral
-	medianContractRequired bool
-	osmContractRequired    bool
+	collateral types.Collateral
 )
 
 var addNewCollateralCmd = &cobra.Command{
@@ -43,23 +39,23 @@ func init() {
 }
 
 func addNewCollateral() error {
-	var collateralErr error
-	collateral, collateralErr = getCollateral()
+	prompter := prompts.NewPrompter()
+	collateral, collateralErr := prompter.GetCollateralDetails()
 	if collateralErr != nil {
 		return collateralErr
 	}
 
-	contracts, getContractsErr := getContracts()
-	if getContractsErr != nil {
-		return getContractsErr
+	contracts, contractsErr := prompter.GetContractDetails()
+	if contractsErr != nil {
+		return contractsErr
 	}
 
-	configUpdater := config.NewConfigUpdater(collateral, contracts, medianContractRequired, osmContractRequired)
+	configUpdater := config.NewConfigUpdater(collateral, contracts, prompter.MedianRequired, prompter.OsmRequired)
 	configFileName := "mcdTransformers"
 	configFilePath := helpers.GetEnvironmentsPath()
 	initializerWriter := initializer.Generator{
 		Collateral:                collateral,
-		MedianInitializerRequired: medianContractRequired,
+		MedianInitializerRequired: prompter.MedianRequired,
 	}
 	newCollateralGenerator := new_collateral.NewCollateralGenerator{
 		ConfigFileName:       configFileName,
@@ -88,153 +84,4 @@ func addNewCollateral() error {
 	}
 
 	return nil
-}
-
-func getCollateral() (types.Collateral, error) {
-	namePrompt := promptui.Prompt{
-		Label:    "Collateral Name",
-		Validate: validateString,
-	}
-
-	versionPrompt := promptui.Prompt{
-		Label:    "Collateral Version",
-		Validate: validateString,
-	}
-
-	collateralName, nameErr := namePrompt.Run()
-	if nameErr != nil {
-		fmt.Printf("Prompt failed %v\n", nameErr)
-		return types.Collateral{}, nameErr
-	}
-	collateralVersion, versionErr := versionPrompt.Run()
-	if versionErr != nil {
-		fmt.Printf("Prompt failed %v\n", versionErr)
-		return types.Collateral{}, versionErr
-	}
-
-	return types.NewCollateral(collateralName, collateralVersion), nil
-}
-
-func validateString(input string) error {
-	if input == "" {
-		return errors.New("This string field is required")
-	}
-	return nil
-}
-
-func validateInt(input string) error {
-	_, err := strconv.Atoi(input)
-	if err != nil {
-		return errors.New("Invalid number")
-	}
-	return nil
-}
-
-func getContracts() (map[string]types.Contract, error) {
-	contracts := make(map[string]types.Contract)
-
-	flipContract, flipErr := getContract("Flip")
-	if flipErr != nil {
-		return contracts, flipErr
-	}
-	contracts["flip"] = flipContract
-
-	contracts, medianErr := addMedianContract(contracts)
-	if medianErr != nil {
-		return contracts, medianErr
-	}
-
-	contracts, osmErr := addOsmContract(contracts)
-	if osmErr != nil {
-		return contracts, osmErr
-	}
-	return contracts, nil
-}
-
-func addMedianContract(contracts map[string]types.Contract) (map[string]types.Contract, error) {
-	var medianRequiredErr error
-	medianContractRequired, medianRequiredErr = isContractRequired("Median")
-	if medianRequiredErr != nil {
-		return contracts, medianRequiredErr
-	}
-
-	if medianContractRequired {
-		medianContract, medianContractErr := getContract("Median")
-		if medianContractErr != nil {
-			return contracts, medianContractErr
-		}
-		contracts["median"] = medianContract
-	}
-
-	return contracts, nil
-}
-
-func addOsmContract(contracts map[string]types.Contract) (map[string]types.Contract, error) {
-	var osmRequiredErr error
-	osmContractRequired, osmRequiredErr = isContractRequired("OSM")
-	if osmRequiredErr != nil {
-		return contracts, osmRequiredErr
-	}
-
-	if osmContractRequired {
-		osmContract, osmContractErr := getContract("OSM")
-		if osmContractErr != nil {
-			return contracts, osmContractErr
-		}
-		contracts["osm"] = osmContract
-	}
-
-	return contracts, nil
-}
-
-func getContract(contractType string) (types.Contract, error) {
-	addressPrompt := promptui.Prompt{
-		Label:    fmt.Sprintf("%s Contract Address", contractType),
-		Validate: validateString,
-	}
-
-	abiPrompt := promptui.Prompt{
-		Label:    fmt.Sprintf("%s Contract ABI", contractType),
-		Validate: validateString,
-	}
-
-	blockPrompt := promptui.Prompt{
-		Label:    fmt.Sprintf("%s Contract deployment block", contractType),
-		Validate: validateInt,
-	}
-
-	address, addressErr := addressPrompt.Run()
-	if addressErr != nil {
-		return types.Contract{}, addressErr
-	}
-	abi, abiErr := abiPrompt.Run()
-	if abiErr != nil {
-		return types.Contract{}, abiErr
-	}
-	blockResult, blockErr := blockPrompt.Run()
-	if blockErr != nil {
-		return types.Contract{}, blockErr
-	}
-	block, intErr := strconv.Atoi(blockResult)
-	if intErr != nil {
-		return types.Contract{}, intErr
-	}
-
-	return types.Contract{
-		Address:  address,
-		Abi:      abi,
-		Deployed: block,
-	}, nil
-}
-
-func isContractRequired(contractType string) (bool, error) {
-	prompt := promptui.Select{
-		Label: fmt.Sprintf("Is a %s contract required?", contractType),
-		Items: []bool{true, false},
-	}
-	_, result, resultErr := prompt.Run()
-	if resultErr != nil {
-		return false, resultErr
-	}
-	return strconv.ParseBool(result)
 }
