426c2ec29d5be7e139a89638574436ed71f9f954
Sam MacPherson <xylom44@gmail.com>
2020-11-01 10:02:47 -0500

    added assert limit of max 100 buckets per query; removed redundant naming

--- db/migrations/20201007080108_create_time_ilk_snapshot_query.sql
+++ db/migrations/20201007080108_create_time_ilk_snapshot_query.sql
@@ -25,39 +25,50 @@ CREATE TYPE api.time_ilk_snapshot AS
 CREATE FUNCTION api.time_ilk_snapshots(ilk_identifier TEXT, range_start TIMESTAMP, range_end TIMESTAMP, bucket_interval INTERVAL DEFAULT '1 day'::INTERVAL)
     RETURNS SETOF api.time_ilk_snapshot AS
 $$
-WITH buckets AS (SELECT generate_series(range_start, range_end - bucket_interval, bucket_interval) AS bucket_start)
-SELECT buckets.bucket_start AS bucket_start,
-       buckets.bucket_start + bucket_interval AS bucket_end,
-       bucket_interval AS bucket_interval,
-       ilk_identifier,
-       block_number,
-       rate,
-       art,
-       spot,
-       line,
-       dust,
-       chop,
-       lump,
-       flip,
-       rho,
-       duty,
-       pip,
-       mat,
-       created,
-       updated
-FROM buckets
-    LEFT JOIN api.ilk_snapshot ON
-    (
-        ilk_snapshot.ilk_identifier = time_ilk_snapshots.ilk_identifier AND
-        block_number = (
-            SELECT MAX(block_number)
-            FROM api.ilk_snapshot
-            WHERE ilk_snapshot.ilk_identifier = time_ilk_snapshots.ilk_identifier AND updated < buckets.bucket_start + bucket_interval
-        )
-    )
-ORDER BY bucket_start
+DECLARE
+    r api.time_ilk_snapshot%rowtype;
+BEGIN
+    ASSERT EXTRACT(EPOCH FROM (range_end - range_start)) / EXTRACT(EPOCH FROM bucket_interval) <= 100, 'Please limit requests to at most 100 buckets.';
+    
+    FOR r IN 
+        WITH buckets AS (SELECT generate_series(range_start, range_end - bucket_interval, bucket_interval) AS bucket_start)
+        SELECT buckets.bucket_start,
+            buckets.bucket_start + bucket_interval AS bucket_end,
+            bucket_interval,
+            time_ilk_snapshots.ilk_identifier,
+            block_number,
+            rate,
+            art,
+            spot,
+            line,
+            dust,
+            chop,
+            lump,
+            flip,
+            rho,
+            duty,
+            pip,
+            mat,
+            created,
+            updated
+        FROM buckets
+            LEFT JOIN api.ilk_snapshot ON
+            (
+                ilk_snapshot.ilk_identifier = time_ilk_snapshots.ilk_identifier AND
+                block_number = (
+                    SELECT MAX(block_number)
+                    FROM api.ilk_snapshot
+                    WHERE ilk_snapshot.ilk_identifier = time_ilk_snapshots.ilk_identifier AND updated < buckets.bucket_start + bucket_interval
+                )
+            )
+        ORDER BY bucket_start
+    LOOP
+        return next r;
+    END LOOP;
+    return;
+END;
 $$
-    LANGUAGE sql
+    LANGUAGE plpgsql
     STRICT
     STABLE;
 
