d308314679a771e6b03b7f59527eaeaac0af2c7a
Eric Smith <paytonrules@gmail.com>
2020-11-13 16:26:35 -0600

    Version MEDIAN_BAL to be MEDIAN_BAL_1_1_14

--- transformers/storage/median/initializers/median_bal/v1_1_14/initializer.go
+++ transformers/storage/median/initializers/median_bal/v1_1_14/initializer.go
@@ -1,11 +1,11 @@
 // This is a plugin generated to export the configured transformer initializers
 
-package median_bal
+package v1_1_14
 
 import (
 	constants "github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	initializers "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 )
 
-var contractAddress = constants.GetContractAddress("MEDIAN_BAL")
+var contractAddress = constants.GetContractAddress("MEDIAN_BAL_1_1_14")
 var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(contractAddress)
