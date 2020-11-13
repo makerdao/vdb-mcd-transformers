645fcc1bb495484891f51fe3a509764b5f6b8881
Eric Smith <paytonrules@gmail.com>
2020-11-13 17:32:27 -0600

    Version MEDIAN_BAT

--- transformers/storage/median/initializers/median_bat/v1_0_0/initializer.go
+++ transformers/storage/median/initializers/median_bat/v1_0_0/initializer.go
@@ -1,8 +1,8 @@
-package median_bat
+package v1_0_0
 
 import (
 	"github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 	"github.com/makerdao/vdb-transformer-utilities/pkg/shared/constants"
 )
 
-var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_BAT"))
+var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_BAT_1_0_0"))
