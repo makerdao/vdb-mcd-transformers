aeeb52bf13247f763cb18bf2b0fdf7eb4b48851c
Eric Smith <paytonrules@gmail.com>
2020-11-13 16:38:59 -0600

    Version the YFI median contract

--- transformers/storage/median/initializers/median_yfi/v1_1_14/initializer.go
+++ transformers/storage/median/initializers/median_yfi/v1_1_14/initializer.go
@@ -1,11 +1,11 @@
 // This is a plugin generated to export the configured transformer initializers
 
-package median_yfi
+package v1_1_14
 
 import (
 	constants "github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	initializers "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 )
 
-var contractAddress = constants.GetContractAddress("MEDIAN_YFI")
+var contractAddress = constants.GetContractAddress("MEDIAN_YFI_1_1_14")
 var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(contractAddress)
