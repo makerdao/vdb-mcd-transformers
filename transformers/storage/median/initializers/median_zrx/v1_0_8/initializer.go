1f0f345a8d3a31262bc68919ab6f7daeb130a30e
Eric Smith <paytonrules@gmail.com>
2020-11-16 15:29:59 -0600

    Version MEDIAN_ZRX
    That's it. They are done. What now what do I do?

--- transformers/storage/median/initializers/median_zrx/v1_0_8/initializer.go
+++ transformers/storage/median/initializers/median_zrx/v1_0_8/initializer.go
@@ -1,8 +1,8 @@
-package median_zrx
+package v1_0_8
 
 import (
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 )
 
-var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_ZRX"))
+var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_ZRX_1_0_8"))
