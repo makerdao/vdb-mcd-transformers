b6dd730f0518d668eeb267022a1e3b6a4423048a
Eric Smith <paytonrules@gmail.com>
2020-11-16 12:06:34 -0600

    Version MEDIAN_KNC

--- transformers/storage/median/initializers/median_knc/v1_0_8/initializer.go
+++ transformers/storage/median/initializers/median_knc/v1_0_8/initializer.go
@@ -1,8 +1,8 @@
-package median_knc
+package v1_0_8
 
 import (
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 )
 
-var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_KNC"))
+var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_KNC_1_0_8"))
