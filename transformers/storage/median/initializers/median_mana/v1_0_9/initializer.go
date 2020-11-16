305786819f75fca1ea5afce089088b72beba695f
Eric Smith <paytonrules@gmail.com>
2020-11-16 14:17:24 -0600

    Verion MANA and USDT

--- transformers/storage/median/initializers/median_mana/v1_0_9/initializer.go
+++ transformers/storage/median/initializers/median_mana/v1_0_9/initializer.go
@@ -1,8 +1,8 @@
-package median_mana
+package v1_0_9
 
 import (
 	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 )
 
-var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_MANA"))
+var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(constants.GetContractAddress("MEDIAN_MANA_1_0_9"))
