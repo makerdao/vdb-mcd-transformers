7724ba949070c6c31476bdd409363e10f9b91b10
Eric Smith <paytonrules@gmail.com>
2020-11-16 13:26:10 -0600

    Version MEDIAN_LINK

--- transformers/test_data/config_values.go
+++ transformers/test_data/config_values.go
@@ -160,7 +160,7 @@ func MedianAddresses() []string {
 		"MEDIAN_COMP_1_1_2",
 		"MEDIAN_ETH_1_0_0",
 		"MEDIAN_KNC_1_0_8",
-		"MEDIAN_LINK",
+		"MEDIAN_LINK_1_1_2",
 		"MEDIAN_LRC",
 		"MEDIAN_MANA",
 		"MEDIAN_WBTC",
@@ -180,7 +180,7 @@ func MedianCompAddress() string {
 }
 func MedianEthAddress() string  { return checksum(constants.GetContractAddress("MEDIAN_ETH_1_0_0")) }
 func MedianKncAddress() string  { return checksum(constants.GetContractAddress("MEDIAN_KNC_1_0_8")) }
-func MedianLinkAddress() string { return checksum(constants.GetContractAddress("MEDIAN_LINK")) }
+func MedianLinkAddress() string { return checksum(constants.GetContractAddress("MEDIAN_LINK_1_1_2")) }
 func MedianLrcAddress() string  { return checksum(constants.GetContractAddress("MEDIAN_LRC")) }
 func MedianManaAddress() string { return checksum(constants.GetContractAddress("MEDIAN_MANA")) }
 func MedianUsdtAddress() string { return checksum(constants.GetContractAddress("MEDIAN_USDT")) }
