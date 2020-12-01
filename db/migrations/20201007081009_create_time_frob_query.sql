8686076e3f49bdc53ca67f9ff44dad02fa776218
Sam MacPherson <xylom44@gmail.com>
2020-12-01 16:01:58 -0500

    fix for time_frob_totals - header id is not gauranteed to be in-order

--- db/migrations/20201007081009_create_time_frob_query.sql
+++ db/migrations/20201007081009_create_time_frob_query.sql
@@ -23,16 +23,8 @@ BEGIN
     ASSERT EXTRACT(EPOCH FROM (range_end - range_start)) / EXTRACT(EPOCH FROM bucket_interval) <= 1000, 'Please limit requests to at most 1000 buckets.';
     
     FOR r IN 
-        WITH buckets AS (SELECT generate_series(range_start, range_end - bucket_interval, bucket_interval) AS bucket_start),
-        buckets_headers AS (
-            SELECT
-                bucket_start,
-                (SELECT id FROM public.headers WHERE block_timestamp >= (extract(epoch FROM bucket_start))::INTEGER ORDER BY block_timestamp LIMIT 1) AS header_id_start,
-                (SELECT id FROM public.headers WHERE block_timestamp <= (extract(epoch FROM bucket_start + bucket_interval))::INTEGER ORDER BY block_timestamp DESC LIMIT 1) AS header_id_end
-            FROM buckets
-        )
-        SELECT buckets_headers.bucket_start,
-            buckets_headers.bucket_start + bucket_interval AS bucket_end,
+        SELECT TO_TIMESTAMP(EXTRACT(EPOCH FROM range_start) + ROUND((headers.block_timestamp - EXTRACT(EPOCH FROM range_start)) / EXTRACT(EPOCH FROM bucket_interval)) * EXTRACT(EPOCH FROM bucket_interval)) AS bucket_start,
+            TO_TIMESTAMP(EXTRACT(EPOCH FROM range_start) + ROUND((headers.block_timestamp - EXTRACT(EPOCH FROM range_start)) / EXTRACT(EPOCH FROM bucket_interval)) * EXTRACT(EPOCH FROM bucket_interval)) + bucket_interval AS bucket_end,
             bucket_interval,
             COUNT(dink) AS count,
             COALESCE(SUM(dink), 0) AS dink,
@@ -41,12 +33,14 @@ BEGIN
             COALESCE(SUM(GREATEST(-dink, 0)), 0) AS free,
             COALESCE(SUM(GREATEST(dart, 0)), 0) AS draw,
             COALESCE(SUM(GREATEST(-dart, 0)), 0) AS wipe
-        FROM buckets_headers
-            LEFT JOIN maker.vat_frob ON (header_id >= header_id_start AND header_id <= header_id_end)
+        FROM maker.vat_frob
+            LEFT JOIN public.headers ON (vat_frob.header_id = headers.id)
             LEFT JOIN maker.urns ON (urns.id = urn_id)
             LEFT JOIN maker.ilks ON (ilks.id = ilk_id)
-            WHERE ilks.identifier = time_frob_totals.ilk_identifier
-        GROUP BY bucket_start, bucket_end, bucket_interval
+            WHERE ilks.identifier = time_frob_totals.ilk_identifier AND
+                headers.block_timestamp >= EXTRACT(EPOCH FROM range_start) and
+                headers.block_timestamp < EXTRACT(EPOCH FROM range_end)
+        GROUP BY bucket_start
         ORDER BY bucket_start
     LOOP
         return next r;
