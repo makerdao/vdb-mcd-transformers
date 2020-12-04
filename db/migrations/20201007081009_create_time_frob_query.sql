348a63e12443fe9a5601876e3579be7ca2c775a0
Sam MacPherson <xylom44@gmail.com>
2020-12-04 08:27:14 -0500

    fixed timezone issue

--- db/migrations/20201007081009_create_time_frob_query.sql
+++ db/migrations/20201007081009_create_time_frob_query.sql
@@ -22,26 +22,38 @@ DECLARE
 BEGIN
     ASSERT EXTRACT(EPOCH FROM (range_end - range_start)) / EXTRACT(EPOCH FROM bucket_interval) <= 1000, 'Please limit requests to at most 1000 buckets.';
 
-    FOR r IN 
-        SELECT TO_TIMESTAMP(EXTRACT(EPOCH FROM range_start) + ROUND((headers.block_timestamp - EXTRACT(EPOCH FROM range_start)) / EXTRACT(EPOCH FROM bucket_interval)) * EXTRACT(EPOCH FROM bucket_interval)) AS bucket_start,
-            TO_TIMESTAMP(EXTRACT(EPOCH FROM range_start) + ROUND((headers.block_timestamp - EXTRACT(EPOCH FROM range_start)) / EXTRACT(EPOCH FROM bucket_interval)) * EXTRACT(EPOCH FROM bucket_interval)) + bucket_interval AS bucket_end,
+    FOR r IN
+        WITH buckets AS (
+            SELECT TO_TIMESTAMP(EXTRACT(EPOCH FROM range_start) + ROUND((headers.block_timestamp - EXTRACT(EPOCH FROM range_start)) / EXTRACT(EPOCH FROM bucket_interval)) * EXTRACT(EPOCH FROM bucket_interval)) AS bucket_start,
+                COUNT(dink) AS count,
+                COALESCE(SUM(dink), 0) AS dink,
+                COALESCE(SUM(dart), 0) AS dart,
+                COALESCE(SUM(GREATEST(dink, 0)), 0) AS lock,
+                COALESCE(SUM(GREATEST(-dink, 0)), 0) AS free,
+                COALESCE(SUM(GREATEST(dart, 0)), 0) AS draw,
+                COALESCE(SUM(GREATEST(-dart, 0)), 0) AS wipe
+            FROM maker.vat_frob
+                LEFT JOIN public.headers ON (vat_frob.header_id = headers.id)
+                LEFT JOIN maker.urns ON (urns.id = urn_id)
+                LEFT JOIN maker.ilks ON (ilks.id = ilk_id)
+                WHERE ilks.identifier = time_frob_totals.ilk_identifier AND
+                    headers.block_timestamp >= EXTRACT(EPOCH FROM range_start) and
+                    headers.block_timestamp < EXTRACT(EPOCH FROM range_end)
+            GROUP BY bucket_start
+            ORDER BY bucket_start
+        )
+        SELECT
+            bucket_start AT TIME ZONE 'UTC' AS bucket_start,
+            (bucket_start + bucket_interval) AT TIME ZONE 'UTC' AS bucket_end,
             bucket_interval,
-            COUNT(dink) AS count,
-            COALESCE(SUM(dink), 0) AS dink,
-            COALESCE(SUM(dart), 0) AS dart,
-            COALESCE(SUM(GREATEST(dink, 0)), 0) AS lock,
-            COALESCE(SUM(GREATEST(-dink, 0)), 0) AS free,
-            COALESCE(SUM(GREATEST(dart, 0)), 0) AS draw,
-            COALESCE(SUM(GREATEST(-dart, 0)), 0) AS wipe
-        FROM maker.vat_frob
-            LEFT JOIN public.headers ON (vat_frob.header_id = headers.id)
-            LEFT JOIN maker.urns ON (urns.id = urn_id)
-            LEFT JOIN maker.ilks ON (ilks.id = ilk_id)
-            WHERE ilks.identifier = time_frob_totals.ilk_identifier AND
-                headers.block_timestamp >= EXTRACT(EPOCH FROM range_start) and
-                headers.block_timestamp < EXTRACT(EPOCH FROM range_end)
-        GROUP BY bucket_start
-        ORDER BY bucket_start
+            count,
+            dink,
+            dart,
+            lock,
+            free,
+            draw,
+            wipe
+        FROM buckets
     LOOP
         return next r;
     END LOOP;
