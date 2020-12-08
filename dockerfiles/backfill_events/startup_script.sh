f5ba7d41da4e421100d0b8856b89a4fcf71a0bb7
Rob Mulholand <rmulholand@8thlight.com>
2020-12-08 14:03:39 -0600

    Switch backfill events base image to buster
    - Avoid alpine since it can hang on plugin.Open

--- dockerfiles/backfill_events/startup_script.sh
+++ dockerfiles/backfill_events/startup_script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "Starting event backfill to block 11376200"
 # Fire up execute
 ./vulcanizedb backfillEvents --config config.toml -e 11376200 # TODO: update before next run
