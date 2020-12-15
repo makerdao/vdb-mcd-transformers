4719f55d57106fd88c4e9456b96f29bc36e0f048
Elizabeth Engelman <emengelman@gmail.com>
2020-12-15 17:12:28 -0500

    Check in db/schema updates

--- db/schema.sql
+++ db/schema.sql
@@ -135,7 +135,8 @@ CREATE TYPE api.flip_bid_snapshot AS (
 	dealt boolean,
 	tab numeric,
 	created timestamp without time zone,
-	updated timestamp without time zone
+	updated timestamp without time zone,
+	flip_address text
 );
 
 
@@ -545,22 +546,24 @@ BEGIN
         WITH ilk_ids AS (SELECT id
                          FROM maker.ilks
                          WHERE identifier = all_flips.ilk),
-             address AS (
-                 SELECT DISTINCT address_id
+             address_ids AS (
+                 SELECT DISTINCT address_id as id
                  FROM maker.flip_ilk
                  WHERE flip_ilk.ilk_id = (SELECT id FROM ilk_ids)
-                 LIMIT 1),
-             bid_ids AS (
-                 SELECT DISTINCT bid_id
+             ),
+             bids AS (
+                 SELECT DISTINCT bid_id, address
                  FROM maker.flip
-                 WHERE address_id = (SELECT * FROM address)
+                 JOIN addresses on addresses.id = maker.flip.address_id
+                 WHERE maker.flip.address_id IN (SELECT * FROM address_ids)
                  ORDER BY bid_id DESC
                  LIMIT CASE WHEN max_results = -1 THEN NULL ELSE max_results END
-                 OFFSET
-                 all_flips.result_offset)
+                     OFFSET
+                     all_flips.result_offset
+        )
         SELECT f.*
-        FROM bid_ids,
-             LATERAL api.get_flip(bid_ids.bid_id, all_flips.ilk) f
+        FROM bids,
+             LATERAL api.get_flip_with_address(bids.bid_id, bids.address, all_flips.ilk) f
     );
 END
 $$;
