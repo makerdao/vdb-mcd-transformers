941998ff2f3061e80c65c50e7ee935c5e5e642f4
Elizabeth Engelman <emengelman@gmail.com>
2020-12-16 15:00:53 -0500

    Fix UNI Median contract name to include version number

--- transformers/storage/median/initializers/median_uni_a/v1_2_1/initializer.go
+++ transformers/storage/median/initializers/median_uni_a/v1_2_1/initializer.go
@@ -1,11 +1,11 @@
 // This is a plugin generated to export the configured transformer initializers
 
-package median_uni_a
+package v1_2_1
 
 import (
 	constants "github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	initializers "github.com/makerdao/vdb-mcd-transformers/transformers/storage/median/initializers"
 )
 
-var contractAddress = constants.GetContractAddress("MEDIAN_UNI_A")
+var contractAddress = constants.GetContractAddress("MEDIAN_UNI_A_1_2_1")
 var StorageTransformerInitializer = initializers.GenerateStorageTransformerInitializer(contractAddress)
