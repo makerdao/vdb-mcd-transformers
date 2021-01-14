d72f5614c1979e12c8a3c6b121f3524a369827c5
Elizabeth Engelman <emengelman@gmail.com>
2021-01-14 11:11:57 -0500

    Add ENDING_BLOCK_NUMBER to backfillEvents startup script

--- dockerfiles/backfill_events/startup_script.sh
+++ dockerfiles/backfill_events/startup_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# The ENDING_BLOCK_NUMBER needs to be set to around the block number where VDB began tracking the collateral(s) events.
+# In other words, just after the block number where the new collateral(s) was added to exeucte.
 if test -z "$ENDING_BLOCK_NUMBER"
 then
     echo ENDING_BLOCK_NUMBER is required and no value was given
     exit 1
 fi
-echo "Starting event backfill from block $ENDING_BLOCK_NUMBER"
+echo "Starting event backfill from block 11602562"
 # Fire up execute
-./vulcanizedb backfillEvents --config config.toml -e $ENDING_BLOCK_NUMBER
+./vulcanizedb backfillEvents --config config.toml -e 11602562
