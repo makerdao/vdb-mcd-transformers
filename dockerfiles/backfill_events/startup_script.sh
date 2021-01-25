b1f999b3543a80c00ce4df7f3537046c3bf5ea4f
Rob Mulholand <rmulholand@8thlight.com>
2021-01-25 17:08:16 -0600

    (VDB-1808) Add event backfill for new collaterals

--- dockerfiles/backfill_events/startup_script.sh
+++ dockerfiles/backfill_events/startup_script.sh
@@ -2,6 +2,6 @@
 
 # The ending block number needs to be set to around the block number where VDB began tracking the collateral(s) events.
 # In other words, just after the block number where the new collateral(s) was added to execute.
-echo "Starting event backfill to block 11602562"
+echo "Starting event backfill to block 11732690"
 # Fire up execute
-./vulcanizedb backfillEvents --config config.toml -e 11602562
+./vulcanizedb backfillEvents --config config.toml -e 11732690
