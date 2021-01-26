1a662671e11d0c52cbd7bad057610065b393739b
Rob Mulholand <rmulholand@8thlight.com>
2021-01-26 13:33:08 -0600

    Remove event backfill

--- plugins/backfill/transformerExporter.go
+++ plugins/backfill/transformerExporter.go
@@ -3,16 +3,6 @@
 package main
 
 import (
-	auction_file "github.com/makerdao/vdb-mcd-transformers/transformers/events/auction_file/initializer"
-	deny "github.com/makerdao/vdb-mcd-transformers/transformers/events/auth/deny_initializer"
-	rely "github.com/makerdao/vdb-mcd-transformers/transformers/events/auth/rely_initializer"
-	deal "github.com/makerdao/vdb-mcd-transformers/transformers/events/deal/initializer"
-	dent "github.com/makerdao/vdb-mcd-transformers/transformers/events/dent/initializer"
-	flip_file_cat "github.com/makerdao/vdb-mcd-transformers/transformers/events/flip_file/cat/initializer"
-	flip_kick "github.com/makerdao/vdb-mcd-transformers/transformers/events/flip_kick/initializer"
-	tend "github.com/makerdao/vdb-mcd-transformers/transformers/events/tend/initializer"
-	tick "github.com/makerdao/vdb-mcd-transformers/transformers/events/tick/initializer"
-	yank "github.com/makerdao/vdb-mcd-transformers/transformers/events/yank/initializer"
 	event "github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	storage "github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
@@ -23,18 +13,7 @@ type exporter string
 var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
-	return []event.TransformerInitializer{
-			deal.EventTransformerInitializer,
-			dent.EventTransformerInitializer,
-			deny.EventTransformerInitializer,
-			flip_file_cat.EventTransformerInitializer,
-			tick.EventTransformerInitializer,
-			yank.EventTransformerInitializer,
-			auction_file.EventTransformerInitializer,
-			flip_kick.EventTransformerInitializer,
-			rely.EventTransformerInitializer,
-			tend.EventTransformerInitializer,
-		},
+	return []event.TransformerInitializer{},
 		[]storage.TransformerInitializer{},
 		[]interface1.ContractTransformerInitializer{}
 }
