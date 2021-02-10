f0c08998fd5adadefab3c4133f0e4ddaf22bff83
Elizabeth Engelman <emengelman@gmail.com>
2021-02-10 16:42:07 -0500

    Update migration formatting

--- db/migrations/20210209134315_create_min_and_max_diff_by_status_query.sql
+++ db/migrations/20210209134315_create_min_and_max_diff_by_status_query.sql
@@ -1,39 +1,57 @@
 -- +goose Up
 
 CREATE TYPE api.min_or_max AS ENUM (
-	'min',
-	'max'
-);
+    'min',
+    'max'
+    );
 
 CREATE TYPE api.storage_diff AS (
-	min_or_max 		api.min_or_max,
-	address 		BYTEA,
-	block_hash 		BYTEA,
-	block_height 	BIGINT,
-	from_backfill 	BOOLEAN,
-	status 			public.diff_status,
-	storage_key 	BYTEA,
-	storage_value 	BYTEA,
-	created 		TIMESTAMP WITHOUT TIME ZONE,
-	updated 		TIMESTAMP WITHOUT TIME ZONE
-);
+    min_or_max api.min_or_max,
+    address BYTEA,
+    block_hash BYTEA,
+    block_height BIGINT,
+    from_backfill BOOLEAN,
+    status public.diff_status,
+    storage_key BYTEA,
+    storage_value BYTEA,
+    created TIMESTAMP WITHOUT TIME ZONE,
+    updated TIMESTAMP WITHOUT TIME ZONE
+    );
 CREATE OR REPLACE FUNCTION api.get_min_and_max_diff_by_status(status TEXT)
-	RETURNS SETOF api.storage_diff
-	LANGUAGE sql
-	STABLE
+    RETURNS SETOF api.storage_diff
+    LANGUAGE sql
+    STABLE
 AS
 $$
-(SELECT 'max'::api.min_or_max as min_or_max, address, block_hash, block_height, from_backfill, status, storage_key, storage_value, created, updated
-		FROM public.storage_diff
- 		WHERE status = LOWER(get_min_and_max_diff_by_status.status)::public.diff_status
-		ORDER BY block_height DESC
-		LIMIT 1)
+(SELECT 'max'::api.min_or_max as min_or_max,
+        address,
+        block_hash,
+        block_height,
+        from_backfill,
+        status,
+        storage_key,
+        storage_value,
+        created,
+        updated
+ FROM public.storage_diff
+ WHERE status = LOWER(get_min_and_max_diff_by_status.status)::public.diff_status
+ ORDER BY block_height DESC
+ LIMIT 1)
 UNION
-(SELECT 'min'::api.min_or_max as min_or_max, address, block_hash, block_height, from_backfill, status, storage_key, storage_value, created, updated
-		FROM public.storage_diff
- 		WHERE status = LOWER(get_min_and_max_diff_by_status.status)::public.diff_status
- 		ORDER BY block_height ASC
-		LIMIT 1)
+(SELECT 'min'::api.min_or_max as min_or_max,
+        address,
+        block_hash,
+        block_height,
+        from_backfill,
+        status,
+        storage_key,
+        storage_value,
+        created,
+        updated
+ FROM public.storage_diff
+ WHERE status = LOWER(get_min_and_max_diff_by_status.status)::public.diff_status
+ ORDER BY block_height ASC
+ LIMIT 1)
 $$;
 
 -- +goose Down
