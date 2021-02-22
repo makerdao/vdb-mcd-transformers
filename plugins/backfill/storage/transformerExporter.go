74a3051ba6d35f7b3d29254a40a4463f1edbe5cc
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-02-22 13:48:48 -0800

    Remove backFill Storage configs

--- plugins/backfill/storage/transformerExporter.go
+++ plugins/backfill/storage/transformerExporter.go
@@ -3,10 +3,6 @@
 package main
 
 import (
-	flip_univ2daiusdc_a_v1_2_5 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/univ2daiusdc_a/v1_2_5"
-	flip_univ2ethusdt_a_v1_2_5 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/univ2ethusdt_a/v1_2_5"
-	flip_univ2linketh_a_v1_2_6 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/univ2linketh_a/v1_2_6"
-	flip_univ2unieth_a_v1_2_6 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/univ2unieth_a/v1_2_6"
 	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	"github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
@@ -18,11 +14,6 @@ var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
 	return []event.TransformerInitializer{},
-		[]storage.TransformerInitializer{
-			flip_univ2daiusdc_a_v1_2_5.StorageTransformerInitializer,
-			flip_univ2ethusdt_a_v1_2_5.StorageTransformerInitializer,
-			flip_univ2linketh_a_v1_2_6.StorageTransformerInitializer,
-			flip_univ2unieth_a_v1_2_6.StorageTransformerInitializer,
-		},
+		[]storage.TransformerInitializer{},
 		[]interface1.ContractTransformerInitializer{}
 }
