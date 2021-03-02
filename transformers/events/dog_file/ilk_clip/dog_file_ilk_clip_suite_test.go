b88a740f6f49a9c50e2a64b28024bf2e8c256829
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-03-01 19:54:13 -0800

    Add initial tests and transformer

---
+++ transformers/events/dog_file/ilk_clip/dog_file_ilk_clip_suite_test.go
@@ -0,0 +1,13 @@
+package ilk_clip_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestDogFileIlkClip(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Dog File Ilk Clip Suite")
+}
