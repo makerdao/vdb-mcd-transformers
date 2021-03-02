3ff6eea7f96b7d2fdee6b6bc45ee15d1b5995a68
Connor Mendenhall <cmendenhall@8thlight.com>
2021-03-02 16:29:28 -0500

    Add UNIV2DAIUSDT-A event backfill config.

--- plugins/backfill/transformerExporter.go
+++ plugins/backfill/transformerExporter.go
@@ -3,6 +3,16 @@
 package main
 
 import (
+	auction_file "github.com/makerdao/vdb-mcd-transformers/transformers/events/auction_file/initializer"
+	deny "github.com/makerdao/vdb-mcd-transformers/transformers/events/auth/deny_initializer"
+	rely "github.com/makerdao/vdb-mcd-transformers/transformers/events/auth/rely_initializer"
+	deal "github.com/makerdao/vdb-mcd-transformers/transformers/events/deal/initializer"
+	dent "github.com/makerdao/vdb-mcd-transformers/transformers/events/dent/initializer"
+	flip_file_cat "github.com/makerdao/vdb-mcd-transformers/transformers/events/flip_file/cat/initializer"
+	flip_kick "github.com/makerdao/vdb-mcd-transformers/transformers/events/flip_kick/initializer"
+	tend "github.com/makerdao/vdb-mcd-transformers/transformers/events/tend/initializer"
+	tick "github.com/makerdao/vdb-mcd-transformers/transformers/events/tick/initializer"
+	yank "github.com/makerdao/vdb-mcd-transformers/transformers/events/yank/initializer"
 	event "github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	storage "github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
@@ -13,7 +23,18 @@ type exporter string
 var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
-	return []event.TransformerInitializer{},
+	return []event.TransformerInitializer{
+			yank.EventTransformerInitializer,
+			tick.EventTransformerInitializer,
+			tend.EventTransformerInitializer,
+			rely.EventTransformerInitializer,
+			flip_kick.EventTransformerInitializer,
+			flip_file_cat.EventTransformerInitializer,
+			deny.EventTransformerInitializer,
+			dent.EventTransformerInitializer,
+			deal.EventTransformerInitializer,
+			auction_file.EventTransformerInitializer,
+		},
 		[]storage.TransformerInitializer{},
 		[]interface1.ContractTransformerInitializer{}
 }
