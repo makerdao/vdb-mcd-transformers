e5fd559cedca267f913ab7e86f956be388c8f6cd
Connor Mendenhall <cmendenhall@8thlight.com>
2021-03-02 18:23:50 -0500

    Remove completed storage backfill.

--- plugins/backfill/storage/transformerExporter.go
+++ plugins/backfill/storage/transformerExporter.go
@@ -3,8 +3,6 @@
 package main
 
 import (
-	flip_univ2aaveeth_a_v1_2_7 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/univ2aaveeth_a/v1_2_7"
-	flip_univ2wbtcdai_a_v1_2_7 "github.com/makerdao/vdb-mcd-transformers/transformers/storage/flip/initializers/univ2wbtcdai_a/v1_2_7"
 	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	"github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
@@ -16,9 +14,6 @@ var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
 	return []event.TransformerInitializer{},
-		[]storage.TransformerInitializer{
-			flip_univ2aaveeth_a_v1_2_7.StorageTransformerInitializer,
-			flip_univ2wbtcdai_a_v1_2_7.StorageTransformerInitializer,
-		},
+		[]storage.TransformerInitializer{},
 		[]interface1.ContractTransformerInitializer{}
 }
