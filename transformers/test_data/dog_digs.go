f362d9d860f05b6aedd5478abc7bf9070fc853ed
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-03-04 15:33:12 -0800

    Adds transformer, entity, and test data

---
+++ transformers/test_data/dog_digs.go
@@ -0,0 +1,63 @@
+package test_data
+
+import (
+	"math/big"
+	"math/rand"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
+	"github.com/makerdao/vulcanizedb/pkg/core"
+)
+
+var (
+	rad = big.NewInt(456)
+
+	RawDogDigsLog = types.Log{
+		Address: common.HexToAddress(Dog1xxAddress()),
+		Topics: []common.Hash{
+			common.HexToHash(constants.DogDigsSignature()),
+			common.HexToHash("0x4554482d41000000000000000000000000000000000000000000000000000000"), // ilk
+		},
+		Data: hexutil.MustDecode("0x" +
+			"00000000000000000000000000000000000000000000000000000000000001c8" + //rad
+			"0000000000000000000000000000000000000000000000000000000000000000" +
+			"0000000000000000000000000000000000000000000000000000000000000000" +
+			"0000000000000000000000000000000000000000000000000000000000000000"),
+		BlockNumber: testBlockNumber,
+		TxHash:      common.Hash{},
+		TxIndex:     0,
+		BlockHash:   common.Hash{},
+		Index:       0,
+		Removed:     false,
+	}
+
+	DogDigsEventLog = core.EventLog{
+		ID:          int64(rand.Int31()),
+		HeaderID:    int64(rand.Int31()),
+		Log:         RawDogDigsLog,
+		Transformed: false,
+	}
+	dogDigsModel = event.InsertionModel{
+		SchemaName: constants.MakerSchema,
+		TableName:  constants.DogDigsTable,
+		OrderedColumns: []event.ColumnName{
+			event.HeaderFK,
+			event.LogFK,
+			event.AddressFK,
+			constants.IlkColumn,
+			constants.RadColumn,
+		},
+		ColumnValues: event.ColumnValues{
+			event.HeaderFK: DogDigsEventLog.HeaderID,
+			event.LogFK:    DogDigsEventLog.ID,
+			//event.AddressFK,
+			//constants.IlkColumn,
+			constants.RadColumn: rad.String(),
+		},
+	}
+)
+
+func DogDigsModel() event.InsertionModel { return CopyModel(dogDigsModel) }
