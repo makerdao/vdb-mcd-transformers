454f94dafcc8f4786c2877a642fee06308671322
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-03-08 00:03:25 -0800

    Adds transformer test and test data

---
+++ transformers/events/dog_deny/entity.go
@@ -0,0 +1,14 @@
+package dog_deny
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type DogDenyEntity struct {
+	Usr       common.Address
+	MsgSender common.Address
+
+	HeaderID        int64
+	LogID           int64
+	ContractAddress common.Address
+}
