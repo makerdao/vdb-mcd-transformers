2688cc72afe6fc408c9cdfa342366a5764398edc
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-03-18 12:42:14 -0700

    Adds test for error when converting log fails

--- transformers/events/clip_kick/transformer_test.go
+++ transformers/events/clip_kick/transformer_test.go
@@ -1,1 +1,22 @@
 package clip_kick_test
+
+import (
+	"github.com/makerdao/vdb-mcd-transformers/test_config"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_kick"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/test_data"
+	"github.com/makerdao/vulcanizedb/pkg/core"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ClipKick transformer", func() {
+	var (
+		db          = test_config.NewTestDB(test_config.NewTestNode())
+		transformer = clip_kick.Transformer{}
+	)
+	It("returns an error if converting the log to an entity fails", func() {
+		_, err := transformer.ToModels("wrong abi", []core.EventLog{test_data.DealEventLog}, db)
+
+		Expect(err).To(HaveOccurred())
+	})
+})
