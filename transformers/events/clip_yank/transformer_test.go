c3e52f13349f0009b5e6712d9f094673560018c9
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-04-07 11:45:33 -0700

    Adds transformer test and transformer code

--- transformers/events/clip_yank/transformer_test.go
+++ transformers/events/clip_yank/transformer_test.go
@@ -3,7 +3,9 @@ package clip_yank_test
 import (
 	"github.com/makerdao/vdb-mcd-transformers/test_config"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_yank"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
 	"github.com/makerdao/vdb-mcd-transformers/transformers/test_data"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	"github.com/makerdao/vulcanizedb/pkg/core"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,4 +22,11 @@ var _ = Describe("ClipYank transformer", func() {
 		Expect(err).To(HaveOccurred())
 	})
 
+	It("converts a log to a Model", func() {
+		models, err := transformer.ToModels(constants.ClipABI(), []core.EventLog{test_data.ClipYankEventLog}, db)
+		Expect(err).NotTo(HaveOccurred())
+		expectedModel := test_data.ClipYankModel()
+		test_data.AssignAddressID(test_data.ClipYankEventLog, expectedModel, db)
+		Expect(models).To(Equal([]event.InsertionModel{expectedModel}))
+	})
 })
