b84917db5c0a94dc30bbcb77924784bbf4114008
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2021-05-03 00:01:58 -0700

    Adds clip take test

---
+++ transformers/integration_tests/clip_take_test.go
@@ -0,0 +1,58 @@
+package integration_tests
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/makerdao/vdb-mcd-transformers/test_config"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_take"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/shared/constants"
+	"github.com/makerdao/vdb-mcd-transformers/transformers/test_data"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
+	"github.com/makerdao/vulcanizedb/libraries/shared/fetcher"
+	"github.com/makerdao/vulcanizedb/pkg/core"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func fetchClipTakeLogsFromChain(config event.TransformerConfig, header core.Header) []core.EventLog {
+	logFetcher := fetcher.NewLogFetcher(blockChain)
+	logs, err := logFetcher.FetchLogs(
+		[]common.Address{common.HexToAddress(config.ContractAddresses[0])},
+		[]common.Hash{common.HexToHash(config.Topic)},
+		header)
+	Expect(err).NotTo(HaveOccurred())
+
+	return test_data.CreateLogs(header.Id, logs, db)
+}
+
+var _ = Describe("Clip Take Transformer", func() {
+	BeforeEach(func() {
+		test_config.CleanTestDB(db)
+	})
+
+	It("fetches and transforms a Clip Take event", func() {
+		blockNumber := int64(12317535)
+
+		clipTakeConfig := event.TransformerConfig{
+			TransformerName:     constants.ClipTakeTable,
+			ContractAddresses:   []string{test_data.Clip130Address()},
+			ContractAbi:         constants.ClipABI(),
+			Topic:               constants.ClipTakeSignature(),
+			StartingBlockNumber: blockNumber,
+			EndingBlockNumber:   blockNumber,
+		}
+
+		header, err := persistHeader(db, blockNumber, blockChain)
+		Expect(err).NotTo(HaveOccurred())
+
+		initializer := event.ConfiguredTransformer{
+			Config:      clipTakeConfig,
+			Transformer: clip_take.Transformer{},
+		}
+		transformer := initializer.NewTransformer(db)
+
+		eventLogs := fetchClipTakeLogsFromChain(clipTakeConfig, header)
+
+		err = transformer.Execute(eventLogs)
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
