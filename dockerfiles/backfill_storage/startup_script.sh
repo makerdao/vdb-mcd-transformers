d7945167e556c133187e86eeecbc2e551e07e98e
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-11-05 09:53:15 -0700

    Makes change to kick off deployment by incrementing end block

--- dockerfiles/backfill_storage/startup_script.sh
+++ dockerfiles/backfill_storage/startup_script.sh
@@ -28,5 +28,5 @@ if test -z "$VDB_PG_CONNECT"; then
 fi
 
 # Run backfillStorage
-echo "Running backfillStorage from block 12318822 to 13425397"
-./vulcanizedb backfillStorage -s=12318822 -e=13425397 --config config.toml
+echo "Running backfillStorage from block 12318822 to 13425398"
+./vulcanizedb backfillStorage -s=12318822 -e=13425398 --config config.toml
