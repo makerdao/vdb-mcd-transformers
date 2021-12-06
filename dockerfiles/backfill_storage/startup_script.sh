bfeeb7bb4828b035f1aa2ccb1aa24f749bdeea9a
grandizzy <grandizzy@localhost>
2021-12-06 09:54:09 +0200

    Do not run backfill storage on every deployment, pass start / end block ro backfill storage as env vars

--- dockerfiles/backfill_storage/startup_script.sh
+++ dockerfiles/backfill_storage/startup_script.sh
@@ -5,7 +5,7 @@
 MISSING_VAR_MESSAGE=" is required and no value was given"
 
 function testDatabaseVariables() {
-  for a in DATABASE_NAME DATABASE_HOSTNAME DATABASE_PORT DATABASE_USER DATABASE_PASSWORD
+  for a in DATABASE_NAME DATABASE_HOSTNAME DATABASE_PORT DATABASE_USER DATABASE_PASSWORD BACKFILL_START_BLOCK BACKFILL_END_BLOCK
   do
     eval arg="$"$a
     test $arg
@@ -29,4 +29,4 @@ fi
 
 # Run backfillStorage
 echo "Running backfillStorage from block 12349311 to 13425398"
-./vulcanizedb backfillStorage -s=12349311 -e=13425398 --config config.toml
+./vulcanizedb backfillStorage -s=$BACKFILL_START_BLOCK -e=$BACKFILL_END_BLOCK --config config.toml
