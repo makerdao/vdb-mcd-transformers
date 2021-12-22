3dbf175b396281edd2f4d2fad4f793ac817efb10
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2021-12-22 08:11:51 -0800

    Update backfill configs

--- plugins/backfill/transformerExporter.go
+++ plugins/backfill/transformerExporter.go
@@ -3,10 +3,6 @@
 package main
 
 import (
-	clip_kick "github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_kick/initializer"
-	clip_redo "github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_redo/initializer"
-	clip_take "github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_take/initializer"
-	clip_yank "github.com/makerdao/vdb-mcd-transformers/transformers/events/clip_yank/initializer"
 	event "github.com/makerdao/vulcanizedb/libraries/shared/factories/event"
 	storage "github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	interface1 "github.com/makerdao/vulcanizedb/libraries/shared/transformer"
@@ -18,10 +14,6 @@ var Exporter exporter
 
 func (e exporter) Export() ([]event.TransformerInitializer, []storage.TransformerInitializer, []interface1.ContractTransformerInitializer) {
 	return []event.TransformerInitializer{
-		clip_kick.EventTransformerInitializer,
-		clip_take.EventTransformerInitializer,
-		clip_redo.EventTransformerInitializer,
-		clip_yank.EventTransformerInitializer,
 		},
 		[]storage.TransformerInitializer{},
 		[]interface1.ContractTransformerInitializer{}
